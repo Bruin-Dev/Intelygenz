9047e9b811aba5a51cdbbaffa9d7e3fdeab43b0e
Sancho Muñoz <sancho.munoz@intelygenz.com>
2019-02-11 17:00:24 +0000

    bruin-bridge scaffold

---
+++ bruin-bridge/README.md
@@ -0,0 +1,15 @@
+# Getting started
+- Install python 3
+- Install pip for python 3
+- Install virtual env for python 3
+
+## Activating pipenv and installing dependencies
+````
+virtualenv bruin-bridge
+source ./env/bin/activate
+pip install virtualenv
+pip install pipenv
+pipenv shell
+pipenv sync
+pip freeze (This one for checking the installation)
+````
--- bruin-bridge/app.py
+++ bruin-bridge/app.py
@@ -5,7 +5,7 @@ from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 async def run(loop):
     nc = NATS()
 
-    await nc.connect("localhost:4222", loop=loop)
+    await nc.connect("nats-streaming:4222", loop=loop)
 
     async def message_handler(msg):
         subject = msg.subject
@@ -51,6 +51,7 @@ async def run(loop):
     await nc.close()
 
 if __name__ == '__main__':
+    print("Bruin bridge alive and ready")
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run(loop))
     loop.close()
