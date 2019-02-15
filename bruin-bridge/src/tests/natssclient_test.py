5a30efd91a24867c73feaea1c0d96255158614ea
Sancho <sancho.munoz@intelygenz.com>
2019-02-15 13:10:54 +0100

    Fix gitlab-ci

--- bruin-bridge/src/tests/natssclient_test.py
+++ bruin-bridge/src/tests/natssclient_test.py
@@ -16,6 +16,6 @@ class NATSSClientTest():
         assert isinstance(nats_s_client.nc, NATS)
         assert isinstance(nats_s_client.sc, STAN)
 
-    @pytest.mark.xfail(reason="Will fail as a reminder of preeliminar project structure")
-    def test_project_is_structured(self):
-        assert "Project structure" == "FALSE"
+    # @pytest.mark.xfail(reason="Will fail as a reminder of preeliminar project structure")
+    # def test_project_is_structured(self):
+    #     assert "Project structure" == "FALSE"
