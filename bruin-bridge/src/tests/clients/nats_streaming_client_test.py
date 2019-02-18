6b21c5d29a3ebc9025dd621fd1724e27ab4830d1
Sancho <sancho.munoz@intelygenz.com>
2019-02-18 17:08:51 +0100

    Update tests. 75% coverage reached

--- bruin-bridge/src/tests/clients/nats_streaming_client_test.py
+++ bruin-bridge/src/tests/clients/nats_streaming_client_test.py
@@ -1,6 +1,7 @@
-import pytest
 from src.application.clients.nats_streaming_client import NatsStreamingClient
+import pytest
 from unittest.mock import MagicMock
+from asynctest import CoroutineMock
 from asgiref.sync import sync_to_async
 from nats.aio.client import Client as NATS
 from stan.aio.client import Client as STAN
@@ -15,3 +16,12 @@ class TestNatsStreamingSClient():
         await nats_s_client.connect_to_nats()
         assert isinstance(nats_s_client.nc, NATS)
         assert isinstance(nats_s_client.sc, STAN)
+
+    @pytest.mark.asyncio
+    async def publish_message_test(self):
+        nats_s_client = NatsStreamingClient()
+        nats_s_client.sc = MagicMock()
+        nats_s_client.sc.publish = CoroutineMock()
+        await nats_s_client.publish_message("Test-topic", "Test-message")
+        assert nats_s_client.sc.publish.called == True
+        assert nats_s_client.sc.publish.await_args[0] == ("Test-topic", b'Test-message')
