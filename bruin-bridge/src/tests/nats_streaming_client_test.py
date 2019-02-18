1a6ae1511f557142247af6f9d675ea2963059e90
Sancho <sancho.munoz@intelygenz.com>
2019-02-18 14:57:50 +0100

    Add configuration files for pytest and coverage

--- bruin-bridge/src/tests/nats_streaming_client_test.py
+++ bruin-bridge/src/tests/nats_streaming_client_test.py
@@ -6,16 +6,12 @@ from nats.aio.client import Client as NATS
 from stan.aio.client import Client as STAN
 
 
-class TestNATSSClient():
+class TestNatsStreamingSClient():
     @pytest.mark.asyncio
-    async def test_is_instance_of(self):
+    async def connect_to_nats_test(self):
         NATS.connect = sync_to_async(MagicMock())
         STAN.connect = sync_to_async(MagicMock())
         nats_s_client = NatsStreamingClient()
         await nats_s_client.connect_to_nats()
         assert isinstance(nats_s_client.nc, NATS)
         assert isinstance(nats_s_client.sc, STAN)
-
-    # @pytest.mark.xfail(reason="Will fail as a reminder of preeliminar project structure")
-    # def test_project_is_structured(self):
-    #     assert "Project structure" == "FALSE"
