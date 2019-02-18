6cbbd2957de7e391fce8f8d7169584a76d32fdc7
Sancho <sancho.munoz@intelygenz.com>
2019-02-18 18:50:02 +0100

    Coverage 100%. Add verbosity to pytest

--- bruin-bridge/src/application/clients/nats_streaming_client.py
+++ bruin-bridge/src/application/clients/nats_streaming_client.py
@@ -5,13 +5,13 @@ from stan.aio.client import Client as STAN
 class NatsStreamingClient:
     nc = None
     sc = None
+    subs = list()
 
     async def connect_to_nats(self):
         # Use borrowed connection for NATS then mount NATS Streaming
         # client on top.
         self.nc = NATS()
         await self.nc.connect(servers=["nats://nats-streaming:4222"])
-
         # Start session with NATS Streaming cluster.
         self.sc = STAN()
         await self.sc.connect("automation-engine-nats", "client-123", nats=self.nc)
@@ -19,18 +19,18 @@ class NatsStreamingClient:
     async def publish_message(self, topic, message):
         await self.sc.publish(topic, message.encode())
 
-    async def _cb(self, msg):
+    def _cb(self, msg): # pragma: no cover
         print("Received a message (seq={}): {}".format(msg.seq, msg.data))
 
     async def register_consumer(self, topic):
-        global sub
         sub = await self.sc.subscribe(topic, start_at='first', cb=self._cb)
+        self.subs.append(sub)
 
     async def close_nats_connections(self):
         # Stop recieving messages
-        await sub.unsubscribe()
+        for sub in self.subs:
+            await sub.unsubscribe()
         # Close NATS Streaming session
         await self.sc.close()
-
         # We are using a NATS borrowed connection so we need to close manually.
         await self.nc.close()
--- bruin-bridge/src/tests/clients/nats_streaming_client_test.py
+++ bruin-bridge/src/tests/clients/nats_streaming_client_test.py
@@ -2,7 +2,6 @@ from src.application.clients.nats_streaming_client import NatsStreamingClient
 import pytest
 from unittest.mock import MagicMock
 from asynctest import CoroutineMock
-from asgiref.sync import sync_to_async
 from nats.aio.client import Client as NATS
 from stan.aio.client import Client as STAN
 
@@ -10,12 +9,17 @@ from stan.aio.client import Client as STAN
 class TestNatsStreamingSClient():
     @pytest.mark.asyncio
     async def connect_to_nats_test(self):
-        NATS.connect = sync_to_async(MagicMock())
-        STAN.connect = sync_to_async(MagicMock())
+        NATS.connect = CoroutineMock()
+        STAN.connect = CoroutineMock()
         nats_s_client = NatsStreamingClient()
         await nats_s_client.connect_to_nats()
         assert isinstance(nats_s_client.nc, NATS)
         assert isinstance(nats_s_client.sc, STAN)
+        assert nats_s_client.nc.connect.called == True
+        assert nats_s_client.sc.connect.called == True
+        assert nats_s_client.nc.connect.await_args[1]["servers"] == ["nats://nats-streaming:4222"]
+        assert nats_s_client.sc.connect.await_args[0] == ("automation-engine-nats", "client-123")
+        assert nats_s_client.sc.connect.await_args[1]["nats"] == nats_s_client.nc
 
     @pytest.mark.asyncio
     async def publish_message_test(self):
@@ -25,3 +29,30 @@ class TestNatsStreamingSClient():
         await nats_s_client.publish_message("Test-topic", "Test-message")
         assert nats_s_client.sc.publish.called == True
         assert nats_s_client.sc.publish.await_args[0] == ("Test-topic", b'Test-message')
+
+    @pytest.mark.asyncio
+    async def register_consumer_test(self):
+        nats_s_client = NatsStreamingClient()
+        nats_s_client.subs.clear()
+        nats_s_client.sc = MagicMock()
+        nats_s_client.sc.subscribe = CoroutineMock()
+        await nats_s_client.register_consumer("Topic")
+        assert nats_s_client.sc.subscribe.await_args[0] == ("Topic", )
+        assert nats_s_client.sc.subscribe.await_args[1]["start_at"] == "first"
+        assert nats_s_client.sc.subscribe.await_args[1]["cb"] == nats_s_client._cb
+
+    @pytest.mark.asyncio
+    async def close_nats_connection_test(self):
+        nats_s_client = NatsStreamingClient()
+        nats_s_client.subs = list()
+        sub = MagicMock()
+        sub.unsubscribe = CoroutineMock()
+        nats_s_client.subs.append(sub)
+        nats_s_client.sc = MagicMock()
+        nats_s_client.nc = MagicMock()
+        nats_s_client.sc.close = CoroutineMock()
+        nats_s_client.nc.close = CoroutineMock()
+        await nats_s_client.close_nats_connections()
+        assert sub.unsubscribe.called == True
+        assert nats_s_client.sc.close.called == True
+        assert nats_s_client.nc.close.called == True
