5d3b1786fbe4e455d149efdd95dd34c7523fa062
Sancho <sancho.munoz@intelygenz.com>
2019-02-27 16:36:51 +0100

    Create overseer and drone for velocloud using base-microservice

---
+++ velocloud-overseer/src/config/config.py
@@ -0,0 +1,17 @@
+# In order to work, this module must be executed in an environment with the environment variables referenced set.
+# use source env in this directory.
+# If you dont have any env files, ask for one they are not in VCS
+import os
+
+NATS_CONFIG = {
+    'servers': [os.environ["NATS_SERVER1"]],
+    'cluster_name': os.environ["NATS_CLUSTER_NAME"],
+    'client_ID': 'base-microservice',
+    'subscriber': {
+        'max_inflight': 6000,
+        'pending_limits': 6000
+    },
+    'publisher': {
+        'max_pub_acks_inflight': 6000
+    }
+}
---
+++ velocloud-overseer/src/config/testconfig.py
@@ -0,0 +1,14 @@
+# You must replicate the structure of config.py, changing os.environ calls for mock values
+
+NATS_CONFIG = {
+    'servers': 'nats://nats-streaming:4222',
+    'cluster_name': 'automation-engine-nats',
+    'client_ID': 'base-microservice',
+    'subscriber': {
+        'max_inflight': 6000,
+        'pending_limits': 6000
+    },
+    'publisher': {
+        'max_pub_acks_inflight': 6000
+    }
+}
