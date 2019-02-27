2424d982a307bd87e77a620ecbf035e3fbf32c8a
Sancho <sancho.munoz@intelygenz.com>
2019-02-27 17:29:36 +0100

    Velocloud overseer produces events with enterpriseid-edgeid

--- velocloud-overseer/src/app.py
+++ velocloud-overseer/src/app.py
@@ -2,23 +2,44 @@ from asyncio import sleep as aiosleep
 from asgiref.sync import async_to_sync
 from config import config
 from igz.packages.nats.clients import NatsStreamingClient
+import velocloud
+import os
 
 
-def print_callback(msg):
-    print(msg)
+def get_all_edges_by_enterprise():
+    velocloud.configuration.verify_ssl = False
+    client = velocloud.ApiClient(host=os.environ["VELOCLOUD_HOST"])
+    client.authenticate(os.environ["VELOCLOUD_USER"], os.environ["VELOCLOUD_PASS"], operator=True)
+    api = velocloud.AllApi(client)
+    try:
+        # res = api.monitoringGetAggregates(body={})
+        # enterprises = list()
+        # for enterprise in res._enterprises:
+        #     enterprises.append(enterprise._id)
+        enterprises = [1]
+        edges_by_enterprise = list()
+        for enterprise in enterprises:
+            fulledges = api.enterpriseGetEnterpriseEdges({"enterpriseId": enterprise})
+            for edge in fulledges:
+                new_edge = {"enterpriseId": enterprise, "id": edge.id}
+                edges_by_enterprise.append(new_edge)
+    except velocloud.rest.ApiException as e:
+        print(e)
+    return edges_by_enterprise
+
+
+async def send_to_nats(edges):
+    nats_s_client = NatsStreamingClient(config)
+    await nats_s_client.connect_to_nats()
+    for edge in edges:
+        await nats_s_client.publish("edge.status.request", repr(edge))
+    await nats_s_client.close_nats_connections()
 
 
 @async_to_sync
 async def run():
-    nats_s_client = NatsStreamingClient(config)
-    await nats_s_client.connect_to_nats()
-    await nats_s_client.publish("basemicroservice", "Some message")
-    await nats_s_client.publish("basemicroservice", "Some message2")
-    await nats_s_client.publish("basemicroservice", "Some message3")
-    await nats_s_client.subscribe(topic="basemicroservice", callback=print_callback)
-    print("Waiting 5 seconds to consume messages...")
-    await aiosleep(5)
-    await nats_s_client.close_nats_connections()
+    edges = get_all_edges_by_enterprise()
+    await send_to_nats(edges)
 
 
 if __name__ == '__main__':
