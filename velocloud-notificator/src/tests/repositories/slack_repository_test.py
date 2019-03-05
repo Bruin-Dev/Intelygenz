03a09c3bb8d1c7c47a65e60f003d42913ae9cfc3
brandon.samudio <brandon.samudio@intelygenz.com>
2019-03-05 14:48:38 -0800

    An addition of test cases for slack_repository.py

---
+++ velocloud-notificator/src/tests/repositories/slack_repository_test.py
@@ -0,0 +1,21 @@
+from unittest.mock import Mock
+from config import testconfig as config
+from repositories.slack_repository import SlackRepository
+
+
+class TestSlackRepository:
+
+    def instantiation_test(self):
+        mock_client = Mock()
+        test_repo = SlackRepository(config, mock_client)
+        assert test_repo._config == config
+
+    def send_to_slack_test(self):
+        test_msg = Mock()
+        mock_client = Mock()
+        test_repo = SlackRepository(config, mock_client)
+        test_repo._slack_client.send_to_slack = Mock()
+        test_repo.send_to_slack(test_msg)
+        assert test_repo._slack_client.send_to_slack.called
+        # Testing if the message that is being sent to send_to_slack is formatted correctly
+        assert test_repo._slack_client.send_to_slack.mock_calls[0][1][0] == {'text': str(test_msg)}
