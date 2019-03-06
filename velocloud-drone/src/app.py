4b841d6d64dce9783a8265ed218f5ae526c146aa
Sancho <sancho.munoz@intelygenz.com>
2019-03-06 20:25:33 +0100

    Add base event bus to velocloud drone

--- velocloud-drone/src/app.py
+++ velocloud-drone/src/app.py
@@ -1,58 +1,86 @@
 from asgiref.sync import async_to_sync
 from config import config
 from igz.packages.nats.clients import NatsStreamingClient
+from igz.packages.eventbus.eventbus import EventBus
+from igz.packages.eventbus.action import ActionWrapper
 import velocloud
 import os
 import asyncio
 
-publisher = NatsStreamingClient(config, "velocloud-drone-publisher")
-velocloud.configuration.verify_ssl = False
-client = velocloud.ApiClient(host=os.environ["VELOCLOUD_HOST"])
-client.authenticate(os.environ["VELOCLOUD_USER"], os.environ["VELOCLOUD_PASS"], operator=True)
-api = velocloud.AllApi(client)
+
+class Actions:
+    event_bus = None
+    velocloud_client = None
+
+    def __init__(self, event_bus: EventBus, velocloud_client):
+        self.event_bus = event_bus
+        self.velocloud_client = velocloud_client
+
+    def _process_edge(self, edgeids):
+        try:
+            res = self.velocloud_client.edgeGetEdge(body=edgeids)
+        except velocloud.rest.ApiException as e:
+            print(e)
+        return res
+
+    def report_edge_status(self, msg):
+        import json
+        edgeids = json.loads(msg.decode("utf-8").replace("\\", ' ').replace("'", '"'))
+        print(f'Processing edge with data {msg}')
+        edge_status = self._process_edge(edgeids)
+        print(f'Got edge status from Velocloud: {edge_status}')
+
+        if edge_status._edgeState is 'CONNECTED':
+            print('Edge seems OK, sending it to topic edge.status.ok')
+            topic = "edge.status.ok"
+        else:
+            print('Edge seems KO, failure! Sending it to topic edge.status.ko')
+            topic = "edge.status.ko"
+        task = asyncio.ensure_future(self.event_bus.publish_message(topic, repr(edge_status)))
+        task.result()
+        pass
 
 
-def process_edge(edgeids):
-    try:
-        res = api.edgeGetEdge(body=edgeids)
-    except velocloud.rest.ApiException as e:
-        print(e)
-    return res
+class Container:
+    velocloud_client = None
+    publisher = None
+    subscriber = None
+    event_bus = None
+    report_edge_action = None
 
+    def setup(self):
+        velocloud.configuration.verify_ssl = False
+        client = velocloud.ApiClient(host=os.environ["VELOCLOUD_HOST"])
+        client.authenticate(os.environ["VELOCLOUD_USER"], os.environ["VELOCLOUD_PASS"], operator=True)
+        self.velocloud_client = velocloud.AllApi(client)
 
-def report_edge_status(msg):
-    import json
-    edgeids = json.loads(msg.decode("utf-8").replace("\\", ' ').replace("'", '"'))
-    print(f'Processing edge with data {msg}')
-    edge_status = process_edge(edgeids)
-    print(f'Got edge status from Velocloud: {edge_status}')
+        self.publisher = NatsStreamingClient(config, "velocloud-drone-publisher")
+        self.subscriber = NatsStreamingClient(config, "velocloud-drone-subscriber")
 
-    if edge_status._edgeState is 'CONNECTED':
-        print('Edge seems OK, sending it to topic edge.status.ok')
-        topic = "edge.status.ok"
-    else:
-        print('Edge seems KO, failure! Sending it to topic edge.status.ko')
-        topic = "edge.status.ko"
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(publisher.publish(topic, repr(edge_status)))
-    loop.close()
+        self.event_bus = EventBus()
+        self.event_bus.add_consumer(self.subscriber, consumer_name="tasks")
+        self.event_bus.set_producer(self.publisher)
 
+        self.report_edge_action = ActionWrapper(Actions(self.event_bus, self.velocloud_client), "report_edge_status")
 
-async def connect_to_event_bus(subscriber):
-    await publisher.connect_to_nats()
-    await subscriber.connect_to_nats()
-    await subscriber.subscribe(topic="edge.status.task", callback=report_edge_status, durable_name="velocloud_drones",
-                               queue='velocloud_drones')
+    async def start(self):
+        await self.event_bus.connect()
+        # await self.event_bus.subscribe_consumer(consumer_name="tasks", topic="edge.status.task",
+        #                                         action_wrapper=self.report_edge_action, durable_name="velocloud_drones",
+        #                                         queue="velocloud_drones")
 
+        await self.event_bus.subscribe_consumer(consumer_name="tasks", topic="edge.status.task",
+                                                action_wrapper=self.report_edge_action)
 
-@async_to_sync
-async def run():
-    subscriber = NatsStreamingClient(config, "velocloud-drone-subscriber")
-    await connect_to_event_bus(subscriber)
+    @async_to_sync
+    async def run(self):
+        self.setup()
+        await self.start()
 
 
 if __name__ == '__main__':
     print("Velocloud drone starting...")
-    run()
+    container = Container()
+    container.run()
     loop = asyncio.new_event_loop()
     loop.run_forever()
