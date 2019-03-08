fb79951a3ecd439340afd2a012b34453bbf8c82c
brandon.samudio <brandon.samudio@intelygenz.com>
2019-03-08 15:06:00 -0800

    Adds a test case for store_stats in the actions_test

--- velocloud-notificator/src/tests/actions/actions_test.py
+++ velocloud-notificator/src/tests/actions/actions_test.py
@@ -19,3 +19,12 @@ class TestActions:
         test_actions._slack_repository.send_to_slack = Mock()
         test_actions.send_to_slack(test_msg)
         assert test_actions._slack_repository.send_to_slack.called
+
+    def store_stats_test(self):
+        test_msg = Mock()
+        mock_slack_repository = Mock()
+        mock_stats_repo = Mock()
+        test_actions = Actions(config, mock_slack_repository, mock_stats_repo)
+        test_actions._statistic_repository.send_to_stats_client = Mock()
+        test_actions.store_stats(test_msg)
+        assert test_actions._statistic_repository.send_to_stats_client.called
