6d6c65c8112466639af58974771afaaacd07f504
brandon.samudio <brandon.samudio@intelygenz.com>
2019-03-18 12:49:10 -0700

    Includes an actual LogClient into the actions_test for drone to assert certain logs were called

--- velocloud-drone/src/tests/action/actions_test.py
+++ velocloud-drone/src/tests/action/actions_test.py
@@ -3,8 +3,10 @@ from unittest.mock import Mock
 from asynctest import CoroutineMock
 from application.actions.actions import Actions
 from igz.packages.eventbus.eventbus import EventBus
+from igz.packages.Logger.logger_client import LoggerClient
 import velocloud
 from collections import namedtuple
+from config import testconfig as config
 
 
 class TestDroneActions:
@@ -14,11 +16,13 @@ class TestDroneActions:
         test_bus = EventBus(logger=mock_logger)
         velocloud_repo = Mock()
         actions = Actions(test_bus, velocloud_repo, mock_logger)
+        assert actions._logger is mock_logger
+        assert test_bus._logger is mock_logger
         assert actions.event_bus is test_bus
         assert actions.velocloud_repository is velocloud_repo
 
     def process_edge_ok_test(self):
-        mock_logger = Mock()
+        mock_logger = ()
         test_bus = EventBus(logger=mock_logger)
         velocloud_repo = Mock()
         velocloud_repo.get_edge_information = Mock(return_value="Edge is OK")
@@ -29,23 +33,27 @@ class TestDroneActions:
         assert velocloud_repo.get_edge_information.called
 
     def process_edge_ko_test(self):
-        mock_logger = Mock()
+        mock_logger = LoggerClient(config).get_logger()
         test_bus = EventBus(logger=mock_logger)
         velocloud_repo = Mock()
         velocloud_repo.get_edge_information = Mock(side_effect=velocloud.rest.ApiException())
         actions = Actions(test_bus, velocloud_repo, mock_logger)
+        actions._logger.exception = Mock()
         edgeis = dict(host="somehost", enterpriseId=19, id=99)
         edge_status = actions._process_edge(edgeis)
+        assert actions._logger.exception.called
         assert edge_status is None
         assert velocloud_repo.get_edge_information.called
 
     @pytest.mark.asyncio
     async def report_edge_status_ko_status_test(self):
-        mock_logger = Mock()
+        mock_logger = LoggerClient(config).get_logger()
         test_bus = EventBus(logger=mock_logger)
         test_bus.publish_message = CoroutineMock()
         velocloud_repo = Mock()
         actions = Actions(test_bus, velocloud_repo, mock_logger)
+        actions._logger.info = Mock()
+        actions._logger.error = Mock()
         edge_status = namedtuple("edge_status", [])
         edge_status._edgeState = 'FAILING'
         actions._process_edge = Mock(return_value=edge_status)
@@ -54,14 +62,18 @@ class TestDroneActions:
         assert actions._process_edge.called
         assert actions._process_edge.call_args[0][0] == dict(SomeIds="ids")
         assert test_bus.publish_message.call_args[0][0] == 'edge.status.ko'
+        assert actions._logger.info.called
+        assert actions._logger.error.called
 
     @pytest.mark.asyncio
     async def report_edge_status_ok_status_test(self):
-        mock_logger = Mock()
+        mock_logger = LoggerClient(config).get_logger()
         test_bus = EventBus(logger=mock_logger)
         test_bus.publish_message = CoroutineMock()
         velocloud_repo = Mock()
         actions = Actions(test_bus, velocloud_repo, mock_logger)
+        actions._logger.info = Mock()
+        actions._logger.error = Mock()
         edge_status = namedtuple("edge_status", [])
         edge_status._edgeState = 'CONNECTED'
         actions._process_edge = Mock(return_value=edge_status)
@@ -70,3 +82,5 @@ class TestDroneActions:
         assert actions._process_edge.called
         assert actions._process_edge.call_args[0][0] == dict(SomeIds="ids")
         assert test_bus.publish_message.call_args[0][0] == 'edge.status.ok'
+        assert actions._logger.info.called
+        assert actions._logger.error.called is False
