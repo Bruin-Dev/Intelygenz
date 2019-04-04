de102f773dbaa3e63329c8e27fc2e5d0f3b56f1b
Sancho <sancho.munoz@intelygenz.com>
2019-04-04 12:50:38 +0200

    Finish removing imports from custompackages

--- custompackages/igzpackages/setup.py
+++ custompackages/igzpackages/setup.py
@@ -5,18 +5,19 @@ NAME = "igzpackages"
 VERSION = "0.0.1"
 
 REQUIRES = [
-            "async-timeout == 3.0.1",
-            "asyncio == 3.4.3",
-            "asyncio-nats-client == 0.8.2",
-            "asyncio-nats-streaming == 0.1.2",
-            "asynctest == 0.12.2",
-            "coverage == 4.5.2",
-            "nats-python==0.4.0",
-            "protobuf==3.6.1",
-            "pytest==4.2.1",
-            "pytest-asyncio==0.10.0",
-            "pytest-cov==2.6.1",
-            ]
+    "async-timeout == 3.0.1",
+    "asyncio == 3.4.3",
+    "asyncio-nats-client == 0.8.2",
+    "asyncio-nats-streaming == 0.1.2",
+    "asynctest == 0.12.2",
+    "nats-python==0.4.0",
+    "protobuf==3.6.1",
+    "pytest==4.2.1",
+    "pytest-asyncio==0.10.0",
+    "pytest-cov==2.6.1",
+    "coverage == 4.5.2",
+
+]
 setup(
     name=NAME,
     version=VERSION,
