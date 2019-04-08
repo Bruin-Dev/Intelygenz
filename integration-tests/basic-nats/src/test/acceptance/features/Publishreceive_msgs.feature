d6cf3c5fccbc8420443b35c70aca35e7d6adc0c8
Roberto Sanchez <roberto.sanchez@intelygenz.com>
2019-04-08 15:41:21 +0200

    add new acceptance test scenario for feature publish/receive

--- integration-tests/basic-nats/src/test/acceptance/features/Publishreceive_msgs.feature
+++ integration-tests/basic-nats/src/test/acceptance/features/Publishreceive_msgs.feature
@@ -3,6 +3,15 @@
 
 Feature: Publish/receive msgs
 
+  Scenario: Publish and receive from some topics
+    Given an event bus
+    When messages are published to the following topics
+      | topic       | message   |
+      | test.topic1 | message 1 |
+      | test.topic2 | message 2 |
+      | test.topic3 | message 3 |
+    Then will receive all messages
+
   Scenario: Receive published message
     Given an event bus
     When a message is published
---
+++ integration-tests/basic-nats/src/test/acceptance/steps/publish_receive_msgs.py
@@ -0,0 +1,86 @@
+import asyncio
+from behave import given, when, then
+
+from config import config
+from igz.packages.eventbus.action import ActionWrapper
+from igz.packages.eventbus.eventbus import EventBus
+from igz.packages.nats.clients import NatsStreamingClient
+
+
+class EventValidator:
+    expected_event = None
+
+    def __init__(self, expected_event):
+        self.expected_event = expected_event
+        self.received_msg = None
+
+    def check_event_cb(self, event):
+        self.received_msg = event.decode("utf-8")
+
+
+async def publish_msg(context):
+    await context.event_bus.publish_message(context.topic, context.expected_event)
+
+
+async def receive_msg(context):
+    await context.event_bus.subscribe_consumer(consumer_name="base-test-consumer", topic=context.topic,
+                                               action_wrapper=context.validate_action,
+                                               start_at='first')
+
+
+@given('an event bus')
+def step_impl(context):
+    loop = asyncio.get_event_loop()
+    consumer = NatsStreamingClient(config, "base-nats-test-consumer")
+    producer = NatsStreamingClient(config, "base-nats-test-producer")
+
+    context.expected_event = "Some event"
+    event_validator = EventValidator(context.expected_event)
+    context.validate_action = ActionWrapper(event_validator, "check_event_cb")
+    context.event_bus = EventBus()
+    context.event_bus.set_producer(producer)
+    context.event_bus.add_consumer(consumer=consumer, consumer_name="base-test-consumer")
+
+    async def bus_connect():
+        await context.event_bus.connect()
+    loop.run_until_complete(bus_connect())
+
+
+@when('a message is published')
+def step_impl(context):
+    context.topic = "test.topic"
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(publish_msg(context))
+
+
+@when('messages are published to the following topics')
+def step_impl(context):
+    for row in context.table:
+        context.topic = row['topic']
+        context.expected_event = row['message']
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(publish_msg(context))
+
+
+@then('will receive the message')
+def step_impl(context):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(receive_msg(context))
+
+    async def bus_disconnect():
+        await context.event_bus.close_connections()
+    loop.run_until_complete(bus_disconnect())
+
+    assert context.validate_action.state_instance.received_msg == context.expected_event
+
+
+@then('will receive all messages')
+def step_impl(context):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(receive_msg(context))
+
+    async def bus_disconnect():
+        await context.event_bus.close_connections()
+    loop.run_until_complete(bus_disconnect())
+
+    assert context.validate_action.state_instance.received_msg == context.expected_event
