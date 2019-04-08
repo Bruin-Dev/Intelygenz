f81904538141f3d49e256c2b85ed38b5e7321a8e
Roberto Sanchez <roberto.sanchez@intelygenz.com>
2019-04-08 11:44:44 +0200

    add first acceptance test, integrate with honestcode and run it in ci

---
+++ utils/honestcode/src/post_behave_result.py
@@ -0,0 +1,54 @@
+import sys
+import os
+import subprocess
+from urllib import request
+
+
+def format_json(json_path, upload_path):
+    command = "cat " + json_path + " | jq '.' > " + upload_path
+    res = None
+
+    try:
+        res = subprocess.Popen(command, stdin=subprocess.PIPE, shell=True)
+    except OSError:
+        print("Error opening result of behave file.")
+        exit(-1)
+
+    res.wait()
+    if res.returncode != 0:
+        print("Error formatting result of behave file")
+        exit(-1)
+
+
+def upload_to_honestcode(json_path, test_hook_key):
+    with open(json_path, 'rb') as f:
+        data = f.read()
+    url = 'https://pro.honestcode.io/api/hooks/tr/{}'.format(test_hook_key)
+    response = request.urlopen(url, data=data)
+    print(response)
+
+
+def remove_process_files(src_path, out_path):
+    if os.path.exists(src_path):
+        os.remove(src_path)
+    else:
+        print("Could not remove uploaded file.")
+
+    if os.path.exists(out_path):
+        os.remove(out_path)
+    else:
+        print("Could not remove uploaded file.")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        src_file = sys.argv[1]
+        test_hook_key = sys.argv[2]
+        output_path = "file_to_upload.json"
+
+        format_json(src_file, output_path)
+        upload_to_honestcode(output_path, test_hook_key)
+        remove_process_files(src_file, output_path)
+    else:
+        print("Not enough args.")
+        exit(-1)
