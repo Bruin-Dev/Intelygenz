765cbe3413e1c29dbcb8304d7e8aec1da570cab9
brandon.samudio <brandon.samudio@intelygenz.com>
2019-04-16 10:41:50 -0700

    Adds quart igz package to base microservice

--- base-microservice/src/app.py
+++ base-microservice/src/app.py
@@ -4,6 +4,7 @@ from igz.packages.eventbus.eventbus import EventBus
 from igz.packages.eventbus.action import ActionWrapper
 from prometheus_client import start_http_server, Summary
 from igz.packages.Logger.logger_client import LoggerClient
+from igz.packages.server.api import QuartServer
 import asyncio
 
 MESSAGES_PROCESSED = Summary('nats_processed_messages', 'Messages processed from NATS')
@@ -33,6 +34,7 @@ class Container:
     event_bus = None
     durable_action = None
     from_first_action = None
+    server = None
 
     async def run(self):
         self.setup()
@@ -57,6 +59,7 @@ class Container:
         self.event_bus.add_consumer(consumer=self.client2, consumer_name="consumer2")
         self.event_bus.add_consumer(consumer=self.client3, consumer_name="consumer3")
         self.event_bus.add_consumer(consumer=self.client4, consumer_name="consumer4")
+        self.server = QuartServer(config)
 
     async def start(self):
         await self.event_bus.connect()
@@ -86,10 +89,14 @@ class Container:
                                                 queue="queue",
                                                 start_at='first')
 
+    async def start_server(self):
+        await self.server.run_server()
+
 
 if __name__ == '__main__':
     logger.info("Base microservice starting...")
     container = Container()
     loop = asyncio.new_event_loop()
     loop.run_until_complete(container.run())
+    asyncio.ensure_future(container.start_server(), loop=loop)
     loop.run_forever()
