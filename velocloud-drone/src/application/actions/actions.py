bef05223e1bd381ef3c2b563eba5b3939aacf763
brandon.samudio <brandon.samudio@intelygenz.com>
2019-04-17 14:53:03 -0700

    Reports edge and link state to promethesus metrics at localhost:9200

--- velocloud-drone/src/application/actions/actions.py
+++ velocloud-drone/src/application/actions/actions.py
@@ -7,11 +7,15 @@ class Actions:
     _event_bus = None
     _velocloud_repository = None
     _logger = None
+    _edge_counter = None
+    _link_counter = None
 
-    def __init__(self, event_bus: EventBus, velocloud_repository, logger):
+    def __init__(self, event_bus: EventBus, velocloud_repository, logger, edge_counter, link_counter):
         self._event_bus = event_bus
         self._velocloud_repository = velocloud_repository
         self._logger = logger
+        self._edge_counter = edge_counter
+        self._link_counter = link_counter
 
     def _process_edge(self, edgeids):
         edge_status = None
@@ -38,9 +42,14 @@ class Actions:
         self._logger.info(f'Processing edge with data {msg}')
         edge_status = self._process_edge(edgeids)
         self._logger.info(f'Got edge status from Velocloud: {edge_status}')
+
+        self._edge_counter.labels(state=edge_status._edgeState).inc()
         link_status = self._process_link(edgeids)
         if link_status != []:
             self._logger.info(f'Got link status from Velocloud: {link_status}')
+            for links in link_status:
+                self._link_counter.labels(state=links._link._state).inc()
+
         if edge_status._edgeState == 'CONNECTED':
             self._logger.info('Edge seems OK, sending it to topic edge.status.ok')
             topic = "edge.status.ok"
