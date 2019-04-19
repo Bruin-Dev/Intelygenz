32ab891ac8317c47265bebd9c76e1998809ca0d8
brandon.samudio <brandon.samudio@intelygenz.com>
2019-04-19 12:47:36 -0700

    adds a Gauge to overseer, and helps us manuelly check if edge processed = the edges reported on the Grafana

--- velocloud-overseer/src/application/actions/actions.py
+++ velocloud-overseer/src/application/actions/actions.py
@@ -6,15 +6,18 @@ class Actions:
     _event_bus = None
     _velocloud_repository = None
     _logger = None
+    _edge_gauge = None
 
-    def __init__(self, event_bus: EventBus, velocloud_repository, logger):
+    def __init__(self, event_bus: EventBus, velocloud_repository, logger, edge_gauge):
         self._event_bus = event_bus
         self._velocloud_repository = velocloud_repository
         self._logger = logger
+        self._edge_gauge = edge_gauge
 
     async def _send_edge_status_tasks(self):
         edges_by_enterprise = self._velocloud_repository.get_all_enterprises_edges_with_host()
         for edge in edges_by_enterprise:
+            self._edge_gauge.inc()
             self._logger.info(f'Edge discovered with data {edge}! Sending it to NATS edge.status.task queue')
             await self._event_bus.publish_message("edge.status.task", repr(edge))
 
@@ -28,3 +31,4 @@ class Actions:
             await self._send_edge_status_tasks()
             self._logger.info("Executed scheduled task: send edge status tasks")
             await asyncio.sleep(seconds)
+            self._edge_gauge.set(0)
--- velocloud-overseer/src/tests/actions/actions_test.py
+++ velocloud-overseer/src/tests/actions/actions_test.py
@@ -11,11 +11,13 @@ class TestOverseerActions:
         mock_logger = Mock()
         test_bus = EventBus(logger=mock_logger)
         velocloud_repo = Mock()
-        actions = Actions(test_bus, velocloud_repo, mock_logger)
+        test_edge_gauge = Mock()
+        actions = Actions(test_bus, velocloud_repo, mock_logger, test_edge_gauge)
         assert actions._event_bus is test_bus
         assert actions._velocloud_repository is velocloud_repo
         assert actions._event_bus._logger is mock_logger
         assert actions._logger is mock_logger
+        assert actions._edge_gauge is test_edge_gauge
 
     @pytest.mark.asyncio
     async def will_generate_tasks_test(self):
@@ -23,38 +25,47 @@ class TestOverseerActions:
         test_bus = EventBus(logger=mock_logger)
         test_bus.publish_message = CoroutineMock()
         velocloud_repo = Mock()
+        test_edge_gauge = Mock()
         edges = ["task1", "task2"]
         velocloud_repo.get_all_enterprises_edges_with_host = Mock(return_value=edges)
-        actions = Actions(test_bus, velocloud_repo, mock_logger)
+        actions = Actions(test_bus, velocloud_repo, mock_logger, test_edge_gauge)
         actions._logger.info = Mock()
+        actions._edge_gauge.inc = Mock()
         await actions._send_edge_status_tasks()
         assert test_bus.publish_message.call_count is len(edges)
         assert actions._logger.info.called
+        assert actions._edge_gauge.inc.called
 
     @pytest.mark.asyncio
     async def will_perform_scheduled_task_test(self):
         mock_logger = Mock()
         test_bus = EventBus(logger=mock_logger)
         velocloud_repo = Mock()
-        actions = Actions(test_bus, velocloud_repo, mock_logger)
+        test_edge_gauge = Mock()
+        actions = Actions(test_bus, velocloud_repo, mock_logger, test_edge_gauge)
         actions._send_edge_status_tasks = CoroutineMock()
+        actions._edge_gauge.set = Mock()
         loop = asyncio.get_event_loop()
         task = asyncio.ensure_future(actions.send_edge_status_task_interval(0.1, False))
-        await asyncio.sleep(0.2)
+        await asyncio.sleep(0.3)
         task.cancel()
         loop.stop()
         assert actions._send_edge_status_tasks.called
+        assert actions._edge_gauge.set.called
 
     @pytest.mark.asyncio
     async def will_perform_scheduled_task_on_start_test(self):
         mock_logger = Mock()
         test_bus = EventBus(logger=mock_logger)
         velocloud_repo = Mock()
-        actions = Actions(test_bus, velocloud_repo, mock_logger)
+        test_edge_gauge = Mock()
+        actions = Actions(test_bus, velocloud_repo, mock_logger, test_edge_gauge)
         actions._send_edge_status_tasks = CoroutineMock()
+        actions._edge_gauge.set = Mock()
         loop = asyncio.get_event_loop()
         task = asyncio.ensure_future(actions.send_edge_status_task_interval(100, True))
         await asyncio.sleep(0.1)
         task.cancel()
         loop.stop()
         assert actions._send_edge_status_tasks.called
+        assert actions._edge_gauge.set.called is False
