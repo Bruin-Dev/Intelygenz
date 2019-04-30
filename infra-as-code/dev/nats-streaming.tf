8fc25f5e32abc094be7eef0c64841919385e474a
Keko <luismesas@gmail.com>
2019-04-30 14:03:11 -0700

    deploy nats server

--- infra-as-code/dev/nats-streaming.tf
+++ infra-as-code/dev/nats-streaming.tf
@@ -1,18 +1,18 @@
-resource "aws_ecr_repository" "automation-nats-streaming" {
-  name = "${var.environment}-nats-streaming"
+resource "aws_ecr_repository" "automation-nats-streaming-server" {
+  name = "${var.environment}-nats-streaming-server"
 }
 
-data "template_file" "automation-nats-streaming" {
+data "template_file" "automation-nats-streaming-server" {
   template = "${file("${path.module}/task-definitions/nats_streaming.json")}"
 
   vars {
-    image = "${aws_ecr_repository.automation-nats-streaming.repository_url}:${var.build_number}"
+    image = "${aws_ecr_repository.automation-nats-streaming-server.repository_url}:${var.build_number}"
   }
 }
 
-resource "aws_ecs_task_definition" "automation-nats-streaming" {
-  family = "${var.environment}-nats-streaming"
-  container_definitions = "${data.template_file.automation-nats-streaming.rendered}"
+resource "aws_ecs_task_definition" "automation-nats-streaming-server" {
+  family = "${var.environment}-nats-streaming-server"
+  container_definitions = "${data.template_file.automation-nats-streaming-server.rendered}"
   requires_compatibilities = [
     "FARGATE"]
   network_mode = "awsvpc"
@@ -22,8 +22,8 @@ resource "aws_ecs_task_definition" "automation-nats-streaming" {
   task_role_arn = "${aws_iam_role.ecs_execution_role.arn}"
 }
 
-resource "aws_alb_target_group" "automation-nats-streaming" {
-  name = "${var.environment}-nats-streaming"
+resource "aws_alb_target_group" "automation-nats-streaming-server" {
+  name = "${var.environment}-nats-streaming-server"
   port = 8222
   protocol = "HTTP"
   vpc_id = "${aws_vpc.automation-vpc.id}"
@@ -36,7 +36,7 @@ resource "aws_alb_target_group" "automation-nats-streaming" {
 
 resource "aws_security_group" "automation-nats-streaming_service" {
   vpc_id = "${aws_vpc.automation-vpc.id}"
-  name = "${var.environment}-nats-streaming"
+  name = "${var.environment}-nats-streaming-server"
   description = "Allow egress from container"
 
   #  egress {
@@ -46,7 +46,7 @@ resource "aws_security_group" "automation-nats-streaming_service" {
   #    cidr_blocks = [
   #      "0.0.0.0/0"]
   #  }
-  
+
   ingress {
     from_port = 8222
     to_port = 8222
@@ -57,14 +57,14 @@ resource "aws_security_group" "automation-nats-streaming_service" {
   }
 
   tags {
-    Name = "${var.environment}-nats-streaming"
+    Name = "${var.environment}-nats-streaming-server"
     Environment = "${var.environment}"
   }
 }
 
-resource "aws_ecs_service" "automation-nats-streaming" {
-  name = "${var.environment}-nats-streaming"
-  task_definition = "${aws_ecs_task_definition.automation-nats-streaming.family}:${aws_ecs_task_definition.automation-nats-streaming.revision}"
+resource "aws_ecs_service" "automation-nats-streaming-server" {
+  name = "${var.environment}-nats-streaming-server"
+  task_definition = "${aws_ecs_task_definition.automation-nats-streaming-server.family}:${aws_ecs_task_definition.automation-nats-streaming-server.revision}"
   desired_count = 1
   launch_type = "FARGATE"
   cluster = "${aws_ecs_cluster.automation.id}"
@@ -79,7 +79,7 @@ resource "aws_ecs_service" "automation-nats-streaming" {
   }
 
   load_balancer {
-    target_group_arn = "${aws_alb_target_group.automation-nats-streaming.arn}"
+    target_group_arn = "${aws_alb_target_group.automation-nats-streaming-server.arn}"
     container_name = "nats-streaming"
     container_port = 8222
   }
