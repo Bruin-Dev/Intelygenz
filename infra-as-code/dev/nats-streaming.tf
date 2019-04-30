8417e05086682cd02f125ffe552a5dfda9077443
Keko <luismesas@gmail.com>
2019-04-30 13:51:32 -0700

    deploy grafana

---
+++ infra-as-code/dev/nats-streaming.tf
@@ -0,0 +1,86 @@
+resource "aws_ecr_repository" "automation-nats-streaming" {
+  name = "${var.environment}-nats-streaming"
+}
+
+data "template_file" "automation-nats-streaming" {
+  template = "${file("${path.module}/task-definitions/nats_streaming.json")}"
+
+  vars {
+    image = "${aws_ecr_repository.automation-nats-streaming.repository_url}:${var.build_number}"
+  }
+}
+
+resource "aws_ecs_task_definition" "automation-nats-streaming" {
+  family = "${var.environment}-nats-streaming"
+  container_definitions = "${data.template_file.automation-nats-streaming.rendered}"
+  requires_compatibilities = [
+    "FARGATE"]
+  network_mode = "awsvpc"
+  cpu = "256"
+  memory = "1024"
+  execution_role_arn = "${aws_iam_role.ecs_execution_role.arn}"
+  task_role_arn = "${aws_iam_role.ecs_execution_role.arn}"
+}
+
+resource "aws_alb_target_group" "automation-nats-streaming" {
+  name = "${var.environment}-nats-streaming"
+  port = 8222
+  protocol = "HTTP"
+  vpc_id = "${aws_vpc.automation-vpc.id}"
+  target_type = "ip"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "automation-nats-streaming_service" {
+  vpc_id = "${aws_vpc.automation-vpc.id}"
+  name = "${var.environment}-nats-streaming"
+  description = "Allow egress from container"
+
+  #  egress {
+  #    from_port = 0
+  #    to_port = 0
+  #    protocol = "-1"
+  #    cidr_blocks = [
+  #      "0.0.0.0/0"]
+  #  }
+  
+  ingress {
+    from_port = 8222
+    to_port = 8222
+    protocol = "TCP"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  tags {
+    Name = "${var.environment}-nats-streaming"
+    Environment = "${var.environment}"
+  }
+}
+
+resource "aws_ecs_service" "automation-nats-streaming" {
+  name = "${var.environment}-nats-streaming"
+  task_definition = "${aws_ecs_task_definition.automation-nats-streaming.family}:${aws_ecs_task_definition.automation-nats-streaming.revision}"
+  desired_count = 1
+  launch_type = "FARGATE"
+  cluster = "${aws_ecs_cluster.automation.id}"
+
+  network_configuration {
+    security_groups = [
+      "${aws_security_group.automation-nats-streaming_service.id}"]
+    subnets = [
+      "${aws_subnet.automation-private_subnet-1a.id}",
+      "${aws_subnet.automation-private_subnet-1b.id}"]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = "${aws_alb_target_group.automation-nats-streaming.arn}"
+    container_name = "nats-streaming"
+    container_port = 8222
+  }
+}
