f3cb466f8324b7eaf74359e9910691a18692b181
Roberto Sanchez <roberto.sanchez@intelygenz.com>
2019-05-03 18:48:38 +0200

    fix basic-infra creation

--- infra-as-code/basic-infra/init.tf
+++ infra-as-code/basic-infra/init.tf
@@ -1,3 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "automation-infrastructure"
+    key = "terraform-automation-basic-infra-dev.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 variable "environment" {
   default = "automation-dev"
 }
