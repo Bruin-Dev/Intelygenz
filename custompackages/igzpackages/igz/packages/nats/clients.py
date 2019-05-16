05cbd591ac4b45ce96f41097c28e741256a6a487
brandon.samudio <brandon.samudio@intelygenz.com>
2019-05-16 14:29:42 -0700

    changes try/catch to if/else

--- custompackages/igzpackages/igz/packages/nats/clients.py
+++ custompackages/igzpackages/igz/packages/nats/clients.py
@@ -39,7 +39,8 @@ class NatsStreamingClient:
             # Use borrowed connection for NATS then mount NATS Streaming
             # client on top.
             self._nc = NATS()
-            await self._nc.connect(servers=self._config["servers"])
+            self._nc._error_cb = self._cb_error
+            await self._nc.connect(servers=self._config["servers"], max_reconnect_attempts=-1)
             # Start session with NATS Streaming cluster.
             self._sc = STAN()
 
@@ -50,12 +51,16 @@ class NatsStreamingClient:
         await connect_to_nats()
 
     async def publish(self, topic, message):
-        try:
-            await self._sc.publish(topic, message.encode())
-        except Exception:
-            await self.close_nats_connections()
-            await self.connect_to_nats()
-            await self._sc.publish(topic, message.encode())
+        @retry(wait=wait_exponential(multiplier=self._config['multiplier'], min=self._config['min'],
+                                     max=self._config['max']))
+        async def publish(topic, message):
+            if self._nc.is_connected:
+                await self._sc.publish(topic, message.encode())
+            else:
+                await self.close_nats_connections()
+                await self.connect_to_nats()
+                await self._sc.publish(topic, message.encode())
+        await publish(topic, message)
 
     async def _cb_with_ack_and_action(self, msg):
         self._logger.info(f'Message received from topic {msg.sub.subject} with sequence {msg.sequence}')
@@ -71,6 +76,7 @@ class NatsStreamingClient:
                 self._topic_action[msg.sub.subject].execute_stateful_action(event)
             await self._sc.ack(msg)
         except Exception:
+            print('here')
             self._logger.exception(f"NATS ClientException in {self._client_id} client happened")
             self._logger.exception(f"Error executing {self._topic_action[msg.sub.subject].execute_stateful_action} "f"")
             self._logger.exception("Won't ACK message")
@@ -88,40 +94,55 @@ class NatsStreamingClient:
             self._logger.exception(f"NATS ClientException in {self._client_id} client happened")
             self._logger.exception(f"Error executing {self._topic_action[msg.sub.subject]} function")
 
+    async def _cb_error(self):
+        self._logger.info('Message not . Raising exception')
+        raise Exception
+
     async def subscribe_action(self, topic, action: ActionWrapper,
                                start_at='first', time=None, sequence=None, queue=None, durable_name=None):
-        self._topic_action[topic] = action
-        try:
 
-            sub = await self._sc.subscribe(topic,
-                                           start_at=start_at,
-                                           time=time,
-                                           sequence=sequence,
-                                           queue=queue,
-                                           durable_name=durable_name,
-                                           cb=self._cb_with_ack_and_action,
-                                           manual_acks=True,
-                                           max_inflight=self._config["subscriber"][
-                                               "max_inflight"],
-                                           pending_limits=self._config["subscriber"][
-                                               "pending_limits"])
-        except Exception:
-            await self.close_nats_connections()
-            await  self.connect_to_nats()
-            sub = await self._sc.subscribe(topic,
-                                           start_at=start_at,
-                                           time=time,
-                                           sequence=sequence,
-                                           queue=queue,
-                                           durable_name=durable_name,
-                                           cb=self._cb_with_ack_and_action,
-                                           manual_acks=True,
-                                           max_inflight=self._config["subscriber"][
-                                               "max_inflight"],
-                                           pending_limits=self._config["subscriber"][
-                                               "pending_limits"])
+        @retry(wait=wait_exponential(multiplier=self._config['multiplier'], min=self._config['min'],
+                                     max=self._config['max']))
+        async def subscribe_action(topic, action: ActionWrapper,
+                                   start_at='first', time=None, sequence=None, queue=None, durable_name=None):
+            self._topic_action[topic] = action
+            try:
+                print('here')
+                sub = await self._sc.subscribe(topic,
+                                               start_at=start_at,
+                                               time=time,
+                                               sequence=sequence,
+                                               queue=queue,
+                                               durable_name=durable_name,
+                                               cb=self._cb_with_ack_and_action,
+                                               manual_acks=True,
+                                               max_inflight=self._config["subscriber"][
+                                                   "max_inflight"],
+                                               pending_limits=self._config["subscriber"][
+                                                   "pending_limits"])
 
-        self._subs.append(sub)
+            except Exception as e:
+                print(e)
+                await self.close_nats_connections()
+                await  self.connect_to_nats()
+
+                sub = await self._sc.subscribe(topic,
+                                               start_at=start_at,
+                                               time=time,
+                                               sequence=sequence,
+                                               queue=queue,
+                                               durable_name=durable_name,
+                                               cb=self._cb_with_ack_and_action,
+                                               manual_acks=True,
+                                               max_inflight=self._config["subscriber"][
+                                                   "max_inflight"],
+                                               pending_limits=self._config["subscriber"][
+                                                   "pending_limits"])
+
+            self._subs.append(sub)
+            print(self._subs)
+
+        await subscribe_action(topic, action, start_at, time, sequence, queue, durable_name)
 
     async def subscribe(self, topic, callback,
                         start_at='first', time=None, sequence=None, queue=None, durable_name=None):
@@ -139,6 +160,7 @@ class NatsStreamingClient:
         self._subs.append(sub)
 
     async def close_nats_connections(self):
+        print('here')
         # Stop recieving messages
         for sub in self._subs:
             await sub.unsubscribe()
