eac4e7ea37c6db001ed68e3c55f52a90421ec9d0
sancho.munoz <sancho.munoz@intelygenz.com>
2019-05-28 15:10:31 -0700

    Implement APScheduler in overseer

--- velocloud-overseer/src/app.py
+++ velocloud-overseer/src/app.py
@@ -8,10 +8,11 @@ import asyncio
 from igz.packages.server.api import QuartServer
 from application.repositories.prometheus_repository import PrometheusRepository
 from velocloud_client.client.velocloud_client import VelocloudClient
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from pytz import utc
 
 
 class Container:
-    logger = None
     velocloud_client = None
     velocloud_repository = None
     publisher = None
@@ -21,8 +22,10 @@ class Container:
     actions = None
     logger = LoggerClient(config).get_logger()
     server = QuartServer(config)
+    scheduler = None
 
     def setup(self):
+        self.scheduler = AsyncIOScheduler(timezone=utc)
         self.velocloud_client = VelocloudClient(config)
         self.velocloud_repository = VelocloudRepository(config, self.logger, self.velocloud_client)
 
@@ -31,13 +34,15 @@ class Container:
         self.prometheus_repository = PrometheusRepository(config)
         self.event_bus.set_producer(self.publisher)
 
-        self.actions = Actions(self.event_bus, self.velocloud_repository, self.logger, self.prometheus_repository)
+        self.actions = Actions(self.event_bus, self.velocloud_repository, self.logger, self.prometheus_repository,
+                               self.scheduler)
 
     async def start(self):
         self.actions.start_prometheus_metrics_server()
         self.velocloud_repository.connect_to_all_servers()
         await self.event_bus.connect()
-        await self.actions.send_edge_status_task_interval(config.OVERSEER_CONFIG['interval_time'], exec_on_start=True)
+        await self.actions.set_edge_status_job(config.OVERSEER_CONFIG['interval_time'], exec_on_start=True)
+        self.scheduler.start()
 
     async def start_server(self):
         await self.server.run_server()
--- velocloud-overseer/src/application/actions/actions.py
+++ velocloud-overseer/src/application/actions/actions.py
@@ -1,18 +1,16 @@
-import asyncio
 from igz.packages.eventbus.eventbus import EventBus
+from apscheduler.util import undefined
+from datetime import datetime
 
 
 class Actions:
-    _event_bus = None
-    _velocloud_repository = None
-    _logger = None
-    _prometheus_repository = None
 
-    def __init__(self, event_bus: EventBus, velocloud_repository, logger, prometheus_repository):
+    def __init__(self, event_bus: EventBus, velocloud_repository, logger, prometheus_repository, scheduler):
         self._event_bus = event_bus
         self._velocloud_repository = velocloud_repository
         self._logger = logger
         self._prometheus_repository = prometheus_repository
+        self._scheduler = scheduler
 
     async def _send_edge_status_tasks(self):
         edges_by_enterprise = self._velocloud_repository.get_all_enterprises_edges_with_host()
@@ -20,23 +18,22 @@ class Actions:
             self._logger.info(f'Edge discovered with data {edge}! Sending it to NATS edge.status.task queue')
             await self._event_bus.publish_message("edge.status.task", repr(edge))
 
-    async def send_edge_status_task_interval(self, seconds, exec_on_start=True):
+    async def set_edge_status_job(self, seconds, exec_on_start=True):
         self._logger.info(f'Scheduled task: send edge status configured to run each {seconds} seconds')
         self._logger.info(f'It will be executed when start = {exec_on_start}')
-        if not exec_on_start:
-            await asyncio.sleep(seconds)
-        while True:
-            self._prometheus_repository.reset_edges_counter()
-            sum = self.sum_edges_all_hosts()
-            self._prometheus_repository.set_cycle_total_edges(sum)
-            self._logger.info("Executing scheduled task: send edge status tasks")
-            await self._send_edge_status_tasks()
-            self._logger.info("Executed scheduled task: send edge status tasks")
-            await asyncio.sleep(seconds)
+        next_run_time = undefined
+        if exec_on_start:
+            next_run_time = datetime.now()
+        self._scheduler.add_job(self._edge_status_job, 'interval', seconds=seconds, next_run_time=next_run_time)
 
-    def sum_edges_all_hosts(self):
-        sum = self._velocloud_repository.get_all_hosts_edge_count()
-        return sum
+    async def _edge_status_job(self):
+        self._prometheus_repository.set_cycle_total_edges(self._sum_edges_all_hosts())
+        self._logger.info("Executing scheduled task: send edge status tasks")
+        await self._send_edge_status_tasks()
+        self._logger.info("Executed scheduled task: send edge status tasks")
+
+    def _sum_edges_all_hosts(self):
+        return self._velocloud_repository.get_all_hosts_edge_count()
 
     def start_prometheus_metrics_server(self):
         self._prometheus_repository.start_prometheus_metrics_server()
