3cb007a3a9001e5fe32da986e636210af2887437
brandon.samudio <brandon.samudio@intelygenz.com>
2019-06-11 15:21:44 -0700

    Revert "Add redis minimal example to base-microservice"
    This reverts commit a936289c

--- base-microservice/src/app.py
+++ base-microservice/src/app.py
@@ -6,7 +6,6 @@ from prometheus_client import start_http_server, Summary
 from igz.packages.Logger.logger_client import LoggerClient
 from igz.packages.server.api import QuartServer
 import asyncio
-from ast import literal_eval
 
 MESSAGES_PROCESSED = Summary('nats_processed_messages', 'Messages processed from NATS')
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
@@ -14,20 +13,10 @@ logger = LoggerClient(config).get_logger()
 
 
 class DurableAction:
-
-    def __init__(self, eventbus):
-        self.event_bus = eventbus
-
     @MESSAGES_PROCESSED.time()
-    async def durable_print_callback(self, msg):
+    def durable_print_callback(self, msg):
         logger.info('DURABLE GROUP')
         logger.info(msg)
-        decoded_msg = msg.decode('utf-8')
-        msg_dict = literal_eval(decoded_msg)
-        for edge in msg_dict['edges']:
-            msg4 = {"request_id": msg_dict['request_id'], "edge": edge}
-            await self.event_bus.publish_message("edge.status.request", repr(msg4))
-            # logger.info(edge)
 
 
 class FromFirstAction:
@@ -53,22 +42,23 @@ class Container:
 
     def setup(self):
         self.client1 = NatsStreamingClient(config, "base-microservice-client", logger=logger)
-        self.client2 = NatsStreamingClient(config, "base-microservice-client", logger=logger)
-        self.client3 = NatsStreamingClient(config, "base-microservice-client", logger=logger)
+        self.client2 = NatsStreamingClient(config, "base-microservice-client2", logger=logger)
+        self.client3 = NatsStreamingClient(config, "base-microservice-client3", logger=logger)
+        self.client4 = NatsStreamingClient(config, "base-microservice-client4", logger=logger)
 
+        base_durable_action = DurableAction()
         base_from_first_action = FromFirstAction()
 
-        self.event_bus = EventBus(logger=logger)
-        base_durable_action = DurableAction(self.event_bus)
-
-        self.durable_action = ActionWrapper(base_durable_action, "durable_print_callback", is_async=True, logger=logger)
+        self.durable_action = ActionWrapper(base_durable_action, "durable_print_callback", logger=logger)
         self.from_first_action = ActionWrapper(base_from_first_action, "first_print_callback", logger=logger)
+
+        self.event_bus = EventBus(logger=logger)
 
         self.event_bus.set_producer(self.client1)
 
         self.event_bus.add_consumer(consumer=self.client2, consumer_name="consumer2")
         self.event_bus.add_consumer(consumer=self.client3, consumer_name="consumer3")
-
+        self.event_bus.add_consumer(consumer=self.client4, consumer_name="consumer4")
         self.server = QuartServer(config)
 
     async def start(self):
@@ -79,22 +69,22 @@ class Container:
         # Generate some requests.
         logger.info('starting metrics loop')
 
-        await self.event_bus.publish_message("edge.list.request", repr({"request_id": "dead",
-                                                                        "filter": [{'host': 'metvco02.mettel.net',
-                                                                                    'enterprise_ids': []
-                                                                                    }]
-                                                                        }
-                                                                       )
-                                             )
+        await self.event_bus.publish_message("topic1", "Message 1")
+        await self.event_bus.publish_message("topic1", "Message 2")
+        await self.event_bus.publish_message("topic1", "Message 3")
 
-        await self.event_bus.subscribe_consumer(consumer_name="consumer2", topic="edge.list.response",
-                                                action_wrapper=self.durable_action,
-                                                durable_name="base",
-                                                queue="base",
+        await self.event_bus.subscribe_consumer(consumer_name="consumer4", topic="topic1",
+                                                action_wrapper=self.from_first_action,
                                                 start_at='first')
 
-        await self.event_bus.subscribe_consumer(consumer_name="consumer3", topic="edge.status.response",
-                                                action_wrapper=self.from_first_action,
+        await self.event_bus.subscribe_consumer(consumer_name="consumer3", topic="topic1",
+                                                action_wrapper=self.durable_action,
+                                                durable_name="name",
+                                                queue="queue",
+                                                start_at='first')
+
+        await self.event_bus.subscribe_consumer(consumer_name="consumer2", topic="topic1",
+                                                action_wrapper=self.durable_action,
                                                 durable_name="name",
                                                 queue="queue",
                                                 start_at='first')
