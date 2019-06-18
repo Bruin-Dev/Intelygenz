3e8a48dd4c79755ae7014c8d1b3d463e9523bd7c
brandon.samudio <brandon.samudio@intelygenz.com>
2019-06-18 13:47:42 -0700

    Fix the way link_status calls to_dict and dumps to json

--- velocloud-bridge/src/application/actions/edge_status_response.py
+++ velocloud-bridge/src/application/actions/edge_status_response.py
@@ -20,11 +20,18 @@ class ReportEdgeStatus:
 
         edge_status = self._velocloud_repository.get_edge_information(edgeids).to_dict()
 
-        link_status = self._velocloud_repository.get_link_information(edgeids).to_dict()
+        link_status = []
+        links = self._velocloud_repository.get_link_information(edgeids)
+        if links is not None:
+            for link in links:
+                link_status.append(link.to_dict())
+        else:
+            link_status = None
+
         status = 200
         if enterprise_name is None or edge_status is None or link_status is None:
             status = 500
 
         edge_status = {"enterprise_name": enterprise_name, "edges": edge_status, "links": link_status}
         msg_dict = {"request_id": request_id, "edge_info": edge_status, "status": status}
-        await self._event_bus.publish_message("edge.status.response", json.dumps(msg_dict))
+        await self._event_bus.publish_message("edge.status.response", json.dumps(msg_dict, default=str))
