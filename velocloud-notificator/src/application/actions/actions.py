761e96b050e26622873d1dffdab0c3473b137210
sancho.munoz <sancho.munoz@intelygenz.com>
2019-07-02 20:40:11 -0700

    Update notificator to answer to response_topic on email request

--- velocloud-notificator/src/application/actions/actions.py
+++ velocloud-notificator/src/application/actions/actions.py
@@ -32,7 +32,7 @@ class Actions:
         if msg_dict["email_data"] is not None and msg_dict["email_data"] != "":
             status = self._email_repository.send_to_email(msg_dict["email_data"])
         notification_response = {"request_id": msg_dict['request_id'], "status": status}
-        await self._event_bus.publish_message("notification.email.response", json.dumps(notification_response))
+        await self._event_bus.publish_message(msg_dict["response_topic"], json.dumps(notification_response))
 
     def set_stats_to_slack_job(self):
         seconds = self._config.SLACK_CONFIG['time']
