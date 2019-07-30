c426cd56674558f875e6140579251ab0cfbc0241
brandon.samudio <brandon.samudio@intelygenz.com>
2019-07-30 16:42:18 -0700

    add more parameters to velocloud event call

--- custompackages/velocloud/example.py
+++ custompackages/velocloud/example.py
@@ -85,7 +85,11 @@ except ApiException as e:
 
 # print("### GETTING ALL EVENTS BY ENTERPRISE ID: SIGNET ###")
 # # Accepts filters and pagination
-# body = {"enterpriseId": 2}
+#
+# # {"host": "mettel.velocloud.net", "enterprise_id": 137, "edge_id": 1602}
+# body = {"enterpriseId": 137, "edgeId": 1602,  "interval": {
+#     "end": datetime.now(utc),
+#     "start": (datetime.now(utc)-timedelta(hours=168))}}
 # try:
 #     res = api.eventGetEnterpriseEvents(body=body)
 #     print(res)
