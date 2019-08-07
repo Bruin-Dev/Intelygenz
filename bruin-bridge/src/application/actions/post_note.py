052eb86ba4a2c39ed4002a0b98e25c3783e8de9e
sancho.munoz <sancho.munoz@intelygenz.com>
2019-08-06 17:43:05 -0700

    Add action files for bruin bridge notes get and post

---
+++ bruin-bridge/src/application/actions/post_note.py
@@ -0,0 +1,7 @@
+class PostNote:
+
+    def __init__(self, logger, config, event_bus, bruin_client):
+        self._config = config
+        self._logger = logger
+        self._event_bus = event_bus
+        self._bruin_client = bruin_client
