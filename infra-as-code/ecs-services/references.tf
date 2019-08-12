78766b2f628f7edc846632ab51ffaaa48c88d52d
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-08-12 13:40:28 +0200

    Fixed error in configuration declaration

--- infra-as-code/ecs-services/references.tf
+++ infra-as-code/ecs-services/references.tf
@@ -1,6 +1,6 @@
 data "terraform_remote_state" "tfstate-dev-resources" {
   backend = "s3"
-  config {
+  config = {
     bucket = "automation-infrastructure"
     region = "us-east-1"
     key = "terraform-${TF_VAR_ENVIRONMENT}-dev.tfstate"
