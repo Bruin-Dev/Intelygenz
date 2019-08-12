87e4e9d81033fbae29687845a51667c71352ae60
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-08-12 13:47:21 +0200

    Added necessary command for access outputs

--- infra-as-code/ecs-services/last-contact-report.tf
+++ infra-as-code/ecs-services/last-contact-report.tf
@@ -25,12 +25,12 @@ resource "aws_ecs_task_definition" "automation-last-contact-report" {
   network_mode = "awsvpc"
   cpu = "256"
   memory = "512"
-  execution_role_arn = "${data.terraform_remote_state.tfstate-dev-resources.ecs_execution_role}"
-  task_role_arn = "${data.terraform_remote_state.tfstate-dev-resources.ecs_execution_role}"
+  execution_role_arn = "${data.terraform_remote_state.tfstate-dev-resources.outputs.ecs_execution_role}"
+  task_role_arn = "${data.terraform_remote_state.tfstate-dev-resources.outputs.ecs_execution_role}"
 }
 
 resource "aws_security_group" "automation-last-contact-report_service" {
-  vpc_id = "${data.terraform_remote_state.tfstate-dev-resources.vpc_automation_id}"
+  vpc_id = "${data.terraform_remote_state.tfstate-dev-resources.outputs.vpc_automation_id}"
   name = "${var.ENVIRONMENT}-last-contact-report"
   description = "Allow egress from container"
 
@@ -78,7 +78,7 @@ resource "aws_service_discovery_service" "last-contact-report" {
   name = "last-contact-report"
 
   dns_config {
-    namespace_id = "${data.terraform_remote_state.tfstate-dev-resources.aws_service_discovery_automation-zone_id}"
+    namespace_id = "${data.terraform_remote_state.tfstate-dev-resources.outputs.aws_service_discovery_automation-zone_id}"
 
     dns_records {
       ttl = 10
@@ -98,13 +98,13 @@ resource "aws_ecs_service" "automation-last-contact-report" {
   task_definition = "${aws_ecs_task_definition.automation-last-contact-report.family}:${aws_ecs_task_definition.automation-last-contact-report.revision}"
   desired_count = 1
   launch_type = "FARGATE"
-  cluster = "${data.terraform_remote_state.tfstate-dev-resources.automation_cluster_id}"
+  cluster = "${data.terraform_remote_state.tfstate-dev-resources.outputs.automation_cluster_id}"
 
   network_configuration {
     security_groups = [
       "${aws_security_group.automation-last-contact-report_service.id}"]
     subnets = [
-      "${data.terraform_remote_state.tfstate-dev-resources.subnet_automation-private-1a}"]
+      "${data.terraform_remote_state.tfstate-dev-resources.outputs.subnet_automation-private-1a}"]
     assign_public_ip = false
   }
 
