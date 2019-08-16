8261f1edc1593fce8c736441a4afa2ae45eff6d0
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-08-16 13:32:47 +0200

    Added commands to deploy what is doing the script

--- nats_check.sh
+++ nats_check.sh
@@ -4,12 +4,14 @@ i=1
 while [ $i -le 30 ]
 do
     sleep 5
-    i=$(( $i + 1 ))
+    nats_task=`ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep "nats"`
+    echo "nats_task is $nats_task"
     nats_status=`ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep "nats" | awk '{ print $6 }'`
-    if [ $nats_status = "HEALTHY" ]; then
+    if [ -z "$nats_status" ] && [ $nats_status = "HEALTHY" ]; then
         echo "$i: NATS server is ready"
         break
     else
         echo "$i: NATS server is not ready yet"
     fi
+    i=$(( $i + 1 ))
 done
