99078e1735b4237d960c0bd473349c2619c660d6
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-08-16 17:36:21 +0200

    Added conditions for select specific task when are two running

--- nats_check.sh
+++ nats_check.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
-
-nats_server_running_tasks=`ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep "nats" | awk '{ print $6 }'`
+#set -x
+nats_server_running_tasks=`ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep "nats" | awk '{ print $5 }' | cut -d':' -f2`
 echo "nats_server_running_tasks are $nats_server_running_tasks"
 for word in $nats_server_running_tasks; do
-    A="$(cut -d':' -f2 <<<$word)"
+    echo "word is $word"
+    # A=`$word | cut -d':' -f2`
+    # echo "A is $A"
     if [ -z "$n_task_1" ]; then
-        n_task_1=$A
+        n_task_1=$word
     elif [ -z "$n_task_2" ]; then
-        n_task_2=$A
+        n_task_2=$word
     fi
 done
-if [ $n_task_1 -gt $n_task_2 ]; then
+if [ ! -z $n_task_1 ] && [ ! -z $n_task_2 ]; then
+    if [ $n_task_1 -gt $n_task_2 ]; then
+        n_task_nats_server=$n_task_1
+    elif [ $n_task_2 -gt $n_task_2]; then
+        n_task_nats_server=$n_task_2
+    fi
+elif [ ! -z $n_task_1 ]; then
     n_task_nats_server=$n_task_1
-elif [ $n_task_2 -gt $n_task_2]; then
+elif [ ! -z $n_task_2 ]; then
     n_task_nats_server=$n_task_2
 fi
 echo "n_task_nats_server is $n_task_nats_server"
@@ -23,9 +31,9 @@ do
     sleep 10
     nats_task=`ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep "nats" | grep $n_task_nats_server`
     echo "$i: nats_task is $nats_task"
-    nats_status=`ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep "nats" | awk '{ print $6 }' | grep $n_task_nats_server`
+    nats_status=`echo $nats_task | awk '{ print $6 }'`
     echo "$i: nats_status is $nats_status"
-    if [ ! -z "$nats_status" ] && [ "$nats_status" == "HEALTHY" ]; then
+    if [ ! -z "$nats_status" ] && [ $nats_status = "HEALTHY" ]; then
         echo "$i: NATS server is ready"
         break
     else
