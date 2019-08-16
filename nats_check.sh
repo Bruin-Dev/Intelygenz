d2e339488ab97b4eb231ed32b84611b40a16b78c
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-08-16 17:51:25 +0200

    Added functions for script that wait until nats_server is ready

--- nats_check.sh
+++ nats_check.sh
@@ -1,34 +1,41 @@
 #!/bin/sh
-#set -x
-nats_server_running_tasks=`ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep "nats" | awk '{ print $5 }' | cut -d':' -f2`
-echo "nats_server_running_tasks are $nats_server_running_tasks"
-for word in $nats_server_running_tasks; do
-    echo "word is $word"
-    # A=`$word | cut -d':' -f2`
-    # echo "A is $A"
-    if [ -z "$n_task_1" ]; then
-        n_task_1=$word
-    elif [ -z "$n_task_2" ]; then
-        n_task_2=$word
-    fi
-done
-if [ ! -z $n_task_1 ] && [ ! -z $n_task_2 ]; then
-    if [ $n_task_1 -gt $n_task_2 ]; then
+
+get_nats_tasks_numbers () {
+    nats_server_running_tasks=`ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep "nats" | awk '{ print $5 }'`
+    echo "nats_server_running_tasks are $nats_server_running_tasks"
+    for word in $nats_server_running_tasks; do
+        echo "word is $word"
+        A=`echo $word | cut -d':' -f2`
+        echo "A is $A"
+        if [ -z "$n_task_1" ]; then
+            n_task_1=$word
+        elif [ -z "$n_task_2" ]; then
+            n_task_2=$word
+        fi
+    done
+}
+
+get_major_nats_task_number () {
+    if [ ! -z $n_task_1 ] && [ ! -z $n_task_2 ]; then
+        if [ $n_task_1 -gt $n_task_2 ]; then
+            n_task_nats_server=$n_task_1
+        elif [ $n_task_2 -gt $n_task_2]; then
+            n_task_nats_server=$n_task_2
+        fi
+    elif [ ! -z $n_task_1 ]; then
         n_task_nats_server=$n_task_1
-    elif [ $n_task_2 -gt $n_task_2]; then
+    elif [ ! -z $n_task_2 ]; then
         n_task_nats_server=$n_task_2
     fi
-elif [ ! -z $n_task_1 ]; then
-    n_task_nats_server=$n_task_1
-elif [ ! -z $n_task_2 ]; then
-    n_task_nats_server=$n_task_2
-fi
-echo "n_task_nats_server is $n_task_nats_server"
+    echo "n_task_nats_server is $n_task_nats_server"
+}
 
 i=1
 while [ $i -le 30 ]
 do
     sleep 10
+    get_nats_tasks_numbers
+    get_major_nats_task_number
     nats_task=`ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep "nats" | grep $n_task_nats_server`
     echo "$i: nats_task is $nats_task"
     nats_status=`echo $nats_task | awk '{ print $6 }'`
