d354c0106e7a1cc1d062d0dc0cdd06d9937e3f3a
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-08-16 18:32:45 +0200

    Added bash to .gitlab-ci.yml job due to sh problems

--- ci-utils/nats_healthcheck.sh
+++ ci-utils/nats_healthcheck.sh
@@ -1,43 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 get_nats_tasks_numbers () {
     nats_server_running_tasks=`ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep "nats" | awk '{ print $5 }'`
     echo "nats_server_running_tasks are $nats_server_running_tasks"
-    for word in $nats_server_running_tasks; do
-        echo "word is $word"
-        A=`echo $word | cut -d':' -f2`
-        echo "A is $A"
-        if [ -z "$n_task_1" ]; then
-            n_task_1=$word
-        elif [ -z "$n_task_2" ]; then
-            n_task_2=$word
-        fi
-    done
-}
-
-get_major_nats_task_number () {
-    if [ ! -z $n_task_1 ] && [ ! -z $n_task_2 ]; then
-        if [ $n_task_1 -gt $n_task_2 ]; then
-            n_task_nats_server=$n_task_1
-        elif [ $n_task_2 -gt $n_task_2]; then
-            n_task_nats_server=$n_task_2
-        fi
-    elif [ ! -z $n_task_1 ]; then
-        n_task_nats_server=$n_task_1
-    elif [ ! -z $n_task_2 ]; then
-        n_task_nats_server=$n_task_2
-    fi
-    echo "n_task_nats_server is $n_task_nats_server"
+    nats_task_major=`echo "${nats_server_running_tasks[*]}" | sort -nr | head -n1`
 }
 
 nats_server_healthcheck () {
     i=1
     while [ $i -le 30 ]
     do
-        sleep 15
+        #sleep 15
         get_nats_tasks_numbers
-        get_major_nats_task_number
-        nats_task=`ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep "nats" | grep $n_task_nats_server`
+        nats_task=`ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep "nats" | grep $nats_task_major`
         echo "$i: nats_task is $nats_task"
         nats_status=`echo $nats_task | awk '{ print $6 }'`
         echo "$i: nats_status is $nats_status"
