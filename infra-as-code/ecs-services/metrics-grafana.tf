b62a9bbefe89d3ea1da59657e66a1bb8de72e602
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-09-09 17:29:00 +0200

    Added healthcheck to target group of grafana

--- infra-as-code/ecs-services/metrics-grafana.tf
+++ infra-as-code/ecs-services/metrics-grafana.tf
@@ -50,6 +50,14 @@ resource "aws_lb_target_group" "automation-metrics-grafana" {
   lifecycle {
     create_before_destroy = true
   }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:3000/"
+    interval            = 30
+  }
 }
 
 resource "aws_security_group" "automation-grafana_service" {
