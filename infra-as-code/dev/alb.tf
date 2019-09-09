86caee69095f7fa1ed49a47bf0998c1283d17d35
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-09-09 13:41:18 +0200

    Fixed error with name for aws lb target_group

--- infra-as-code/dev/alb.tf
+++ infra-as-code/dev/alb.tf
@@ -88,7 +88,7 @@ resource "aws_lb_listener" "front_end" {
 }
 
 resource "aws_lb_target_group" "automation-front_end" {
-  name = "${var.ENVIRONMENT}-front_end"
+  name = "${var.ENVIRONMENT}-frontend"
   port = 80
   protocol = "HTTP"
   vpc_id = "${aws_vpc.automation-vpc.id}"
