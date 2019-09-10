15e356ce31f59176d80b1170f9081d42eb5f3944
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-09-10 08:54:11 +0200

    Added necessary parameters in health_check for aws_lb_target_group used in grafana

--- infra-as-code/ecs-services/metrics-grafana.tf
+++ infra-as-code/ecs-services/metrics-grafana.tf
@@ -55,8 +55,10 @@ resource "aws_lb_target_group" "automation-metrics-grafana" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:3000/"
     interval            = 30
+    port                = 3000
+    matcher             = 200
+    protocol            = "HTTP"
   }
 }
 
