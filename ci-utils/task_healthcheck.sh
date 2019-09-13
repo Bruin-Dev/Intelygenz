441040e5bc75a25c22c3f046b8e28ab124c9c237
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-09-13 13:53:35 +0200

    -Add command for install bash in healthcheck of grafana. -Add arguments to use new task_healthcheck. -Add MetTel IPs to allowed in ALB

---
+++ ci-utils/task_healthcheck.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+source $(dirname "$0")/common_functions.sh
+
+task_major_number () {
+    if [ "$TASK" == *"prometheus"* ] || [ "$TASK" == *"grafana"* ] || [ "$TASK" == *"notifier"* ]; then
+        tasks_running=$(ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep $TASK | awk '{ print $4 }')
+    else
+        tasks_running=$(ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep $TASK | awk '{ print $5 }')
+    fi
+    if [ -z "$tasks_running" ]; then
+        s_err "No $TASK task exists"
+        exit 1
+    else
+        s_info "$TASK Running tasks are the following"
+        iterate_over_lines $tasks_running
+    fi
+    task_running_major=$(echo "${tasks_running[*]}" | sort -nr | head -n1)
+    s_info "$TASK Running task with major identifier is $task_running_major"
+}
+
+wait_task_healthy () {
+    i=1
+    while [ $i -le 10 ]
+    do
+        sleep 30
+        if [ $i -eq 1 ]; then
+            task_major_number
+        fi
+        nats_task=$(ecs-cli ps --cluster $TF_VAR_ENVIRONMENT --desired-status RUNNING | grep $task_running_major)
+        s_info "Try $i. Waiting for the task with task definition $task_running_major to be in HEALTHY state"
+        if [ "$TASK" == *"prometheus"* ] || [ "$TASK" == *"grafana"* ] || [ "$TASK" == *"notifier"* ]; then
+            nats_status=$(echo $nats_task | awk '{ print $5 }')
+        else 
+            nats_status=$(echo $nats_task | awk '{ print $6 }')
+        fi
+        s_info "Try $i. Health Status of $TASK task is $nats_status"
+        if [ ! -z "$nats_status" ] && [ $nats_status = "HEALTHY" ]; then
+            s_info "Try $i. $TASK task is ready"
+            break
+        else
+            s_info "Try $i. $TASK task is not ready yet"
+        fi
+        i=$(( $i + 1 ))
+    done
+}
+
+task_healthcheck () {
+    wait_task_healthy
+    if [ $i -ge 10 ]; then
+        s_err "$TASK task $task_running_major hasn't reached HEALTHY state in 5 minutes. Aborting job"
+        exit 1
+    fi
+}
+
+while getopts t: option 
+do
+    case "${option}" in
+        t) TASK=${OPTARG};;
+    esac
+done
+
+task_healthcheck
