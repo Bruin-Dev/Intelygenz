e393ec11b3e029cb84e9bb7ed7f2a09180c94d07
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-09-18 20:40:19 +0200

    Fixed error in script

--- ci-utils/environment_assign.sh
+++ ci-utils/environment_assign.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ ${CI_COMMIT_REF_SLUG} == "master" ]]; then
     export ENVIRONMENT_SLUG=master
-elif
+else
     export ENVIRONMENT_SLUG=$(echo -n "${CI_COMMIT_REF_SLUG}" | sha256sum | cut -c1-8)
 fi
