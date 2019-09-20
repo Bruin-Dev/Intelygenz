2f3cc88d91d60367ca4d2c4d0456a3dce541f76a
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-09-20 09:34:52 +0200

    Add public IP Xoan's home Madrid

--- infra-as-code/dev/alb.tf
+++ infra-as-code/dev/alb.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "automation-dev-inbound" {
       "83.61.20.90/32", // IGZ CALLAO OFFICE 1
       "83.61.8.95/32",  // IGZ CALLAO OFFICE 2
       "83.56.7.26/32",   // IGZ CALLAO OFFICE 3
-      "83.40.63.125/32", // XOAN HOME MADRID
+      "88.22.100.205/32", // XOAN HOME MADRID
       "87.219.160.222/32" // XOAN HOME GALICIA
     ]
   }
@@ -34,7 +34,7 @@ resource "aws_security_group" "automation-dev-inbound" {
       "83.61.20.90/32", // IGZ CALLAO OFFICE 1
       "83.61.8.95/32",  // IGZ CALLAO OFFICE 2
       "83.56.7.26/32",  // IGZ CALLAO OFFICE 3
-      "83.40.63.125/32", // XOAN HOME MADRID
+      "88.22.100.205/32", // XOAN HOME MADRID
       "87.219.160.222/32", // XOAN HOME GALICIA
       "54.73.0.183/32", // IGZ GITLAB RUNNERS
       "216.194.50.82/32" // METTEL OFFICE
@@ -52,7 +52,7 @@ resource "aws_security_group" "automation-dev-inbound" {
       "83.61.20.90/32", // IGZ CALLAO OFFICE 1
       "83.61.8.95/32",  // IGZ CALLAO OFFICE 2
       "83.56.7.26/32",  // IGZ CALLAO OFFICE 3
-      "83.40.63.125/32", // XOAN HOME MADRID
+      "88.22.100.205/32", // XOAN HOME MADRID
       "87.219.160.222/32", // XOAN HOME GALICIA
       "54.73.0.183/32", // IGZ GITLAB RUNNERS
       "216.194.50.82/32" // METTEL OFFICE
