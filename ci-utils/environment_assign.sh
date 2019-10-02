60bb367a5d54ad0668e3edc4fec0cc57ef9ff2bf
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-10-02 14:52:43 +0200

    Change variable for ENVIRONMENT_VAR used in Grafana and Prometheus configuration URLs

--- ci-utils/environment_assign.sh
+++ ci-utils/environment_assign.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ ${CI_COMMIT_REF_SLUG} == "master" ]]; then
-    export ENVIRONMENT_VAR="master"
+    export ENVIRONMENT_VAR="automation-master"
 else
-    export ENVIRONMENT_VAR=$(echo -n "${CI_COMMIT_REF_SLUG}" | sha256sum | cut -c1-8)
+    export ENVIRONMENT_VAR="automation-"$(echo -n "${CI_COMMIT_REF_SLUG}" | sha256sum | cut -c1-8)
 fi
