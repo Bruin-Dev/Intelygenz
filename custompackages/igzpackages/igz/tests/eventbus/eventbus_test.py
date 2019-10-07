8d9ca9d93508e557247596f640f67bb4a5467816
Daniel Fernández <daniel.fernandez@intelygenz.com>
2019-10-07 18:22:17 +0200

    Fix tests of the event bus to comply with the new implementation

--- custompackages/igzpackages/igz/tests/eventbus/eventbus_test.py
+++ custompackages/igzpackages/igz/tests/eventbus/eventbus_test.py
@@ -1,6 +1,6 @@
 from igz.packages.eventbus.eventbus import EventBus
 from igz.packages.eventbus.action import ActionWrapper
-from igz.packages.nats.clients import NatsStreamingClient
+from igz.packages.nats.clients import NATSClient
 import pytest
 from asynctest import CoroutineMock
 from igz.config import testconfig as config
@@ -12,115 +12,141 @@ class TestEventBus:
 
     def instantiation_test(self):
         mock_logger = Mock()
-        e = EventBus()
-        e2 = EventBus(logger=mock_logger)
-        assert isinstance(e._consumers, dict)
-        assert isinstance(e._logger, logging._loggerClass) is True
-        assert e._logger.hasHandlers() is True
-        assert e._logger.getEffectiveLevel() is 10
-        assert e2._logger is mock_logger
+        event_bus1 = EventBus()
+        event_bus2 = EventBus(logger=mock_logger)
+
+        assert isinstance(event_bus1._consumers, dict)
+        assert isinstance(event_bus1._logger, logging._loggerClass)
+        assert event_bus1._logger.hasHandlers() is True
+        assert event_bus1._logger.getEffectiveLevel() == 10
+        assert event_bus2._logger is mock_logger
 
     @pytest.mark.asyncio
     async def connect_OK_test(self):
         mock_logger = Mock()
-        e = EventBus(logger=mock_logger)
-        subscriber = NatsStreamingClient(config, "Some-subs-ID", logger=mock_logger)
-        publisher = NatsStreamingClient(config, "Some-pub-ID", logger=mock_logger)
-
+        event_bus = EventBus(logger=mock_logger)
+        subscriber = NATSClient(config, logger=mock_logger)
+        publisher = NATSClient(config, logger=mock_logger)
         sub_connect_mock = CoroutineMock()
         pub_connect_mock = CoroutineMock()
 
         subscriber.connect_to_nats = sub_connect_mock
         publisher.connect_to_nats = pub_connect_mock
 
-        e.add_consumer(subscriber, "Some-name")
-        e.set_producer(publisher)
+        event_bus.add_consumer(subscriber, consumer_name="Some-name")
+        event_bus.set_producer(publisher)
 
-        await e.connect()
-        assert sub_connect_mock.called
-        assert pub_connect_mock.called
+        await event_bus.connect()
+
+        sub_connect_mock.assert_awaited_once()
+        pub_connect_mock.assert_awaited_once()
 
     @pytest.mark.asyncio
     async def rpc_request_test(self):
         mock_logger = Mock()
-        e = EventBus(logger=mock_logger)
-        publisher = NatsStreamingClient(config, "Some-pub-ID", logger=mock_logger)
+        event_bus = EventBus(logger=mock_logger)
+        publisher = NATSClient(config, logger=mock_logger)
         publisher.rpc_request = CoroutineMock()
-        e.set_producer(publisher)
-        rpc_response = await e.rpc_request("some_topic", "some_message", 10)
-        assert publisher.rpc_request.called
-        assert publisher.rpc_request.call_args[0][0] == "some_topic"
-        assert publisher.rpc_request.call_args[0][1] == "some_message"
-        assert publisher.rpc_request.call_args[0][2] == 10
+
+        event_bus.set_producer(publisher)
+
+        await event_bus.rpc_request(
+            topic="some_topic", message="some_message", timeout=10
+        )
+
+        publisher.rpc_request.assert_awaited_once_with(
+            "some_topic", "some_message", 10
+        )
 
     def add_consumer_OK_test(self):
         mock_logger = Mock()
-        e = EventBus(logger=mock_logger)
-        subscriber = NatsStreamingClient(config, "Some-subs-ID", logger=mock_logger)
-        e.add_consumer(subscriber, "some-name")
+        event_bus = EventBus(logger=mock_logger)
+        subscriber = NATSClient(config, logger=mock_logger)
 
-        assert e._consumers["some-name"] is subscriber
+        event_bus.add_consumer(subscriber, consumer_name="some-name")
+
+        assert event_bus._consumers["some-name"] is subscriber
 
     def add_consumer_KO_repeated_test(self):
         mock_logger = Mock()
-        e = EventBus(logger=mock_logger)
-        e._logger.error = Mock()
-        first_subscriber = NatsStreamingClient(config, "Some-subs-ID", logger=mock_logger)
-        second_subscriber = NatsStreamingClient(config, "Some-subs-ID-2", logger=mock_logger)
-        e.add_consumer(first_subscriber, "some-name")
-        e.add_consumer(second_subscriber, "some-name")
-        assert e._consumers["some-name"] is first_subscriber
-        assert e._consumers["some-name"] is not second_subscriber
-        assert e._logger.error.called
+        event_bus = EventBus(logger=mock_logger)
+        first_subscriber = NATSClient(config, logger=mock_logger)
+        second_subscriber = NATSClient(config, logger=mock_logger)
+        event_bus._logger.error = Mock()
+
+        event_bus.add_consumer(first_subscriber, consumer_name="some-name")
+        event_bus.add_consumer(second_subscriber, consumer_name="some-name")
+
+        event_bus._logger.error.assert_called()
+        assert event_bus._consumers["some-name"] is first_subscriber
+        assert event_bus._consumers["some-name"] is not second_subscriber
 
     def set_producer_OK_test(self):
         mock_logger = Mock()
-        e = EventBus(logger=mock_logger)
-        publisher = NatsStreamingClient(config, "Some-pub-ID", logger=mock_logger)
-        e.set_producer(publisher)
-        assert e._producer is publisher
+        event_bus = EventBus(logger=mock_logger)
+        publisher = NATSClient(config, logger=mock_logger)
+
+        event_bus.set_producer(publisher)
+
+        assert event_bus._producer is publisher
 
     @pytest.mark.asyncio
     async def subscribe_consumer_OK_test(self):
         mock_logger = Mock()
-        e = EventBus(logger=mock_logger)
-        subscriber = NatsStreamingClient(config, "Some-subs-ID", logger=mock_logger)
+        event_bus = EventBus(logger=mock_logger)
+        subscriber = NATSClient(config, logger=mock_logger)
+        action_mock = ActionWrapper(
+            state_instance=None,
+            target_function="",
+            logger=mock_logger,
+        )
         subscribe_action_mock = CoroutineMock()
-        action_mock = ActionWrapper(None, "", logger=mock_logger)
         subscriber.subscribe_action = subscribe_action_mock
-        e.add_consumer(subscriber, "Some-name")
-        await e.subscribe_consumer("Some-name", "Some-topic", action_mock)
-        assert subscribe_action_mock.called
-        assert subscribe_action_mock.call_args[0] == ('Some-topic', action_mock, 'first', None, None, None, None, 30)
+
+        event_bus.add_consumer(subscriber, consumer_name="Some-name")
+        await event_bus.subscribe_consumer(
+            consumer_name="Some-name",
+            topic="Some-topic",
+            action_wrapper=action_mock,
+            queue="",
+        )
+
+        subscribe_action_mock.assert_awaited_once_with(
+            "Some-topic", action_mock, "",
+        )
 
     @pytest.mark.asyncio
     async def publish_message_OK_test(self):
         mock_logger = Mock()
-        e = EventBus(logger=mock_logger)
-        publisher = NatsStreamingClient(config, "Some-pub-ID", logger=mock_logger)
+        event_bus = EventBus(logger=mock_logger)
+        publisher = NATSClient(config, logger=mock_logger)
         publish_mock = CoroutineMock()
         publisher.publish = publish_mock
-        e.set_producer(publisher)
-        await e.publish_message("some-topic", "some-message")
-        assert publish_mock.called
-        assert publish_mock.call_args[0] == ("some-topic", "some-message")
+
+        event_bus.set_producer(publisher)
+        await event_bus.publish_message(
+            topic="some-topic", msg="some-message"
+        )
+
+        publish_mock.assert_awaited_once_with(
+            "some-topic", "some-message",
+        )
 
     @pytest.mark.asyncio
     async def close_connections_OK_test(self):
         mock_logger = Mock()
-        e = EventBus(logger=mock_logger)
-        subscriber = NatsStreamingClient(config, "Some-subs-ID", logger=mock_logger)
-        publisher = NatsStreamingClient(config, "Some-pub-ID", logger=mock_logger)
-
+        event_bus = EventBus(logger=mock_logger)
+        subscriber = NATSClient(config, logger=mock_logger)
+        publisher = NATSClient(config, logger=mock_logger)
         sub_disconnect_mock = CoroutineMock()
         pub_disconnect_mock = CoroutineMock()
-
         subscriber.close_nats_connections = sub_disconnect_mock
         publisher.close_nats_connections = pub_disconnect_mock
 
-        e.add_consumer(subscriber, "Some-name")
-        e.set_producer(publisher)
+        event_bus.add_consumer(subscriber, consumer_name="Some-name")
+        event_bus.set_producer(publisher)
 
-        await e.close_connections()
-        assert sub_disconnect_mock.called
-        assert pub_disconnect_mock.called
+        await event_bus.close_connections()
+
+        sub_disconnect_mock.assert_awaited()
+        pub_disconnect_mock.assert_awaited()
