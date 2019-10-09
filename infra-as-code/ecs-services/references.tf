dd0cb29ac15a346ee1df005a4cfb245edfeba9c0
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-10-09 17:07:21 +0200

    Change declaration of variable ENVIRONMENT to TF_VAR_ENVIRONMENT in 'infra-as-code/ecs-services/references.tf' file

--- infra-as-code/ecs-services/references.tf
+++ infra-as-code/ecs-services/references.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "tfstate-dev-resources" {
   config = {
     bucket = "automation-infrastructure"
     region = "us-east-1"
-    key = "terraform-${var.ENVIRONMENT}-dev-tests-network-resources.tfstate"
+    key = "terraform-${TF_VAR_ENVIRONMENT}-dev-tests-network-resources.tfstate"
   }
 }
 
