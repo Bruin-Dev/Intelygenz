713fac97de3b2c7a583080d6a6bfb8b13f46547b
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-10-10 17:08:34 +0200

    fix(Change of subnetmask of private and public ips)

--- infra-as-code/network-resources/variables.tf
+++ infra-as-code/network-resources/variables.tf
@@ -9,32 +9,32 @@ variable "cidr_base" {
 variable "cdir_public_1" {
   type = "map"
   default = {
-    "production"  = "10.1.1.0/16"
-    "dev"         = "10.2.1.0/16"
+    "production"  = "10.1.1.0/24"
+    "dev"         = "10.2.1.0/24"
   }
 }
 
 variable "cdir_public_2" {
   type = "map"
   default = {
-    "production"  = "10.1.2.0/16"
-    "dev"         = "10.2.2.0/16"
+    "production"  = "10.1.2.0/24"
+    "dev"         = "10.2.2.0/24"
   }
 }
 
 variable "cdir_private_1" {
   type = "map"
   default = {
-    "production"  = "10.1.11.0/16"
-    "dev"         = "10.2.11.0/16"
+    "production"  = "10.1.11.0/24"
+    "dev"         = "10.2.11.0/24"
   }
 }
 
 variable "cdir_private_2" {
   type = "map"
   default = {
-    "production"  = "10.1.12.0/16"
-    "dev"         = "10.2.12.0/16"
+    "production"  = "10.1.12.0/24"
+    "dev"         = "10.2.12.0/24"
   }
 }
 
