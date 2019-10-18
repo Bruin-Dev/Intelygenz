6a1e784b1438d3c20cbffabefabc24b0774ba3c3
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-10-18 15:22:50 +0200

    fix(Change service name with repeated word automation)

--- infra-as-code/metrics-dashboards-alarms/locals.tf
+++ infra-as-code/metrics-dashboards-alarms/locals.tf
@@ -1,7 +1,13 @@
 locals {
+  // metrics local variables
+  exception_detected_metric-metric_transformation-name = "ExceptionMessagesDetectedInServices"
+
   // dashboards local variables
   cluster_dashboard_name = "cluster-${var.ENVIRONMENT}"
 
   // alarms local variables
   cluster_task_running-alarm_name = "tasks_running-${var.ENVIRONMENT}"
+
+  // cloudfourmation local variables
+  stack_alarms-name = "sns_topic_alarm_stack"
 }
