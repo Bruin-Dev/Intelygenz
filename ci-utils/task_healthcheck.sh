5ebf651b7ce4092aaf408e684665685770321361
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-10-30 19:00:10 +0100

    Add condition for nats-server

--- ci-utils/task_healthcheck.sh
+++ ci-utils/task_healthcheck.sh
@@ -6,7 +6,7 @@ task_major_number () {
     if [[ ${TASK} == *prometheus* ]] || [[ ${TASK} == *grafana* ]] || [[ ${TASK} == *notifier* ]]; then
         tasks_running=$(ecs-cli ps --cluster ${TF_VAR_ENVIRONMENT} --desired-status RUNNING | grep ${TASK} | awk '{ print $4 }')
     elif [[ ${TASK} == *nats-server* ]]; then
-         tasks_running=$(ecs-cli ps --cluster ${TF_VAR_ENVIRONMENT} --desired-status RUNNING | grep nats-server | grep -v nats-server- | awk '{ print $5 }')
+         tasks_running=$(ecs-cli ps --cluster ${TF_VAR_ENVIRONMENT} --desired-status RUNNING | grep nats-server | grep -v nats-server- | awk '{ print $6 }')
     else
         tasks_running=$(ecs-cli ps --cluster ${TF_VAR_ENVIRONMENT} --desired-status RUNNING | grep ${TASK} | awk '{ print $5 }')
     fi
