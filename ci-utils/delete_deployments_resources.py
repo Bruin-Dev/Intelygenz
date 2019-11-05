332c53fa6759b059e3c3c4cdc433dd8278401f08
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-11-05 16:54:51 +0100

    feat(Add functions for delete target groups and alb used by cluster)

--- ci-utils/delete_deployments_resources.py
+++ ci-utils/delete_deployments_resources.py
@@ -95,7 +95,7 @@ def delete_services_in_namespace(cluster, services):
                                                                                      cluster))
             subprocess.call(
                 ['aws', 'servicediscovery', 'delete-service', '--id', services[index]["service_id"], '--region',
-                 'us-east-1'])
+                 'us-east-1'], stdout=FNULL)
 
 
 def delete_namespace_service(cluster, namespace_id):
@@ -180,15 +180,69 @@ def delete_redis_cluster(cluster):
         logging.error("Redis cluster {} doesn't exists".format(cluster))
 
 
+def check_alb_exists(cluster):
+    alb_exists = {'exists': False}
+    alb_list_call = subprocess.Popen(
+        ['aws', 'elbv2', 'describe-load-balancers', '--region', 'us-east-1'],
+        stdout=subprocess.PIPE, stderr=FNULL)
+    alb_list = json.loads(alb_list_call.stdout.read())
+    albs = alb_list['LoadBalancers']
+    if len(albs) > 0:
+        for element in albs:
+            if element['LoadBalancerName'] == cluster and element['Type'] == 'application':
+                alb_exists.update({'exists': True, 'alb_information': element})
+    return alb_exists
+
+
+def get_targets_groups_for_alb_and_cluster(alb_arn, cluster):
+    target_groups = []
+    target_group_list_call = subprocess.Popen(
+        ['aws', 'elbv2', 'describe-target-groups', '--region', 'us-east-1'],
+        stdout=subprocess.PIPE, stderr=FNULL)
+    target_group_list = json.loads(target_group_list_call.stdout.read())['TargetGroups']
+    for element in target_group_list:
+        if element['LoadBalancerArns']:
+            for targetgroup_alb_arn in element['LoadBalancerArns']:
+                if alb_arn == targetgroup_alb_arn:
+                    target_groups.append(element)
+        elif cluster in element['TargetGroupName']:
+            target_groups.append(element)
+    return target_groups
+
+
+def delete_targets_group_for_alb(alb_arn, cluster):
+    target_groups_to_delete = get_targets_groups_for_alb_and_cluster(alb_arn, cluster)
+    if len(target_groups_to_delete) > 0:
+        for i in range(len(target_groups_to_delete)):
+            target_group_arn = target_groups_to_delete[i]['TargetGroupArn']
+            target_group_name = target_groups_to_delete[i]['TargetGroupName']
+            logging.info("Deleting target group with name {} and arn {}".format(target_group_arn, target_group_name ))
+            subprocess.call(['aws', 'elbv2', 'delete-target-group', '--target-group-arn', target_group_arn], stdout=FNULL)
+
+
+
+def delete_alb(cluster):
+    alb_exists = check_alb_exists(cluster)
+    if alb_exists['exists']:
+        logging.info("ALB with name {} exists".format(cluster))
+        alb_arn = alb_exists['alb_information']['LoadBalancerArn']
+        logging.info("ALB cluster ARN is: {}".format(alb_arn))
+        logging.info("Deleting alb with name {} and arn {}".format(cluster, alb_arn))
+        subprocess.call(['aws', 'elbv2', 'delete-load-balancer', '--load-balancer-arn', alb_arn], stdout=FNULL)
+        delete_targets_group_for_alb(alb_arn, cluster)
+    else:
+        logging.error("ALB with name {} doesn't exists".format(cluster))
+
+
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv, "hc:s:e:r:", ["cluster=", "servicediscovery", "ecs-cluster", "redis-cluster"])
+        opts, args = getopt.getopt(argv, "hc:s:e:r:a:", ["cluster=", "servicediscovery", "ecs-cluster", "redis-cluster", "alb"])
     except getopt.GetoptError:
-        print('delete_deployments_resources.py -c <ecs_cluster_identifier> [-e] [-s] [-r]')
+        print('delete_deployments_resources.py -c <ecs_cluster_identifier> [-e] [-s] [-r] [-a]')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('delete_deployments_resources.py -c <ecs_cluster_identifier> [-e] [-s] [-r]')
+            print('delete_deployments_resources.py -c <ecs_cluster_identifier> [-e] [-s] [-r] [-a]')
             sys.exit()
         elif opt in ("-c", "--cluster"):
             cluster = arg
@@ -198,6 +252,8 @@ def main(argv):
             delete_ecs_cluster(cluster)
         elif opt in ("-r", "--redis-cluster"):
             delete_redis_cluster(cluster)
+        elif opt in ("-a", "--alb"):
+            delete_alb(cluster)
 
 
 if __name__ == "__main__":
