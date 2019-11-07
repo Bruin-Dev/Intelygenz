405511d1bfe52b632e234fae25f9f54991190474
sancho.munoz <sancho.munoz@intelygenz.com>
2019-11-07 14:31:17 -0800

    Add 8 more edges for service affecting. Add scaffold for the 4 we need contact information for.

--- service-affecting-monitor/src/config/contact_info.py
+++ service-affecting-monitor/src/config/contact_info.py
@@ -1,22 +1,162 @@
 devices_by_id = [
 
-   {
-            "serial": 'VC05200033383',
-            "host": "mettel.velocloud.net",
-            "enterprise_id": 137,
-            "edge_id": 1651,
-            "email": "gclark@titanamerica.com",
-            "phone": "757-533-7151",
-            "name": "Gary Clark"
+    {
+        # TA VA - Campostella RM
+        "serial": 'VC05200033383',
+        "host": "mettel.velocloud.net",
+        "enterprise_id": 137,
+        "edge_id": 1651,
+        "email": "gclark@titanamerica.com",
+        "phone": "757-533-7151",
+        "name": "Gary Clark"
     },
-   {
-            "serial": 'VC05200037717',
-            "host": "mettel.velocloud.net",
-            "enterprise_id": 137,
-            "edge_id": 2279,
-            "email": "Rbrown@titanamerica.com",
-            "phone": "954-770-8824 ",
-            "name": "Radley Brown"
+    {
+        # TA FL - Ft Lauderdale
+        "serial": 'VC05200037717',
+        "host": "mettel.velocloud.net",
+        "enterprise_id": 137,
+        "edge_id": 2279,
+        "email": "Rbrown@titanamerica.com",
+        "phone": "954-770-8824",
+        "name": "Radley Brown"
 
-   },
+    },
+    {
+        # ASK BRIAN, site name is 5700 Lake Wright Drive, Norfolk. Site name TA Corp - Twin Oaks Complex
+        # Other contacts in norfolk are Gary Clark or Jeff Mack. Are any of those?
+        # TA Corp - Twin Oaks Complex
+        # "serial": 'VC05200026138',
+        # "host": "mettel.velocloud.net",
+        # "enterprise_id": 137,
+        # "edge_id": 958,
+        # "email": "UNKNOWN",
+        # "phone": "UNKNOWN",
+        # "name": "UNKNOWN"
+
+    },
+    {
+        # TA FL - Deerfield Beach
+        "serial": 'VC05400001548',
+        "host": "mettel.velocloud.net",
+        "enterprise_id": 137,
+        "edge_id": 1502,
+        "email": "DBern@titanamerica.com",
+        "phone": "954-425-4167",
+        "name": "David Bern"
+
+    },
+    {
+        # TA FL - Deerfield Beach BACKUP
+        "serial": 'VC05400001485',
+        "host": "mettel.velocloud.net",
+        "enterprise_id": 137,
+        "edge_id": 1502,
+        "email": "DBern@titanamerica.com",
+        "phone": "954-425-4167",
+        "name": "David Bern"
+
+    },
+    {
+        # TA FL - Pennsuco-Medley
+        "serial": 'VC05200033081',
+        "host": "mettel.velocloud.net",
+        "enterprise_id": 137,
+        "edge_id": 1515,
+        "email": "tsuarez@titanamerica.com",
+        "phone": "305-827-7458",
+        "name": "Tony Suarrez"
+
+    },
+    {
+        # TA VA - Suffolk RM
+        "serial": 'VC05200029594',
+        "host": "mettel.velocloud.net",
+        "enterprise_id": 137,
+        "edge_id": 1573,
+        "email": "rjustice@titanamerica.com",
+        "phone": "757-923-0183",
+        "name": "Robert Justice"
+
+    },
+    {
+        # TA VA - Troutville (RCC - Plant)
+        "serial": 'VC05200038423',
+        "host": "mettel.velocloud.net",
+        "enterprise_id": 137,
+        "edge_id": 1997,
+        "email": "gludwick@titanamerica.com",
+        "phone": "540-765-3145",
+        "name": "Greg Ludwick"
+
+    },
+    {
+        # TA FL - Greenbay Davenport FL
+        "serial": 'VC05200037161',
+        "host": "mettel.velocloud.net",
+        "enterprise_id": 137,
+        "edge_id": 2135,
+        "email": "srivera@titanamerica.com",
+        "phone": "Off # 863-420-8289, Cell# 321-662-8255",
+        "name": "Sonny Rivera"
+
+    },
+    {
+        # TA VA - Summerfield
+        "serial": 'VC05200039471',
+        "host": "mettel.velocloud.net",
+        "enterprise_id": 137,
+        "edge_id": 2151,
+        "email": "jaylor@titanamerica.com",
+        "phone": "540-512-7663",
+        "name": "Jennifer Aylor"
+
+    },
+    {
+        # # NOT SURE ASK BRIAN
+        # # TA Corp - QTS Data Center
+        # "serial": 'VCE20000000940',
+        # "host": "mettel.velocloud.net",
+        # "enterprise_id": 137,
+        # "edge_id": 2419,
+        # "email": "jrossi@west.com ",
+        # "phone": "502-618-5535",
+        # "name": "Justin Rossi"
+
+    },
+    {
+        # # NOT SURE ASK BRIAN
+        # # TA Corp - QTS Data Center BACKUP DEVICE
+        # "serial": 'VCE20000000895',
+        # "host": "mettel.velocloud.net",
+        # "enterprise_id": 137,
+        # "edge_id": 2419,
+        # "email": "jrossi@west.com ",
+        # "phone": "502-618-5535",
+        # "name": "Justin Rossi"
+
+    },
+    {
+        # ASK BRIAN
+        # # TA FL - Zephyrhills RM
+        # "serial": 'VC05200059766',
+        # "host": "mettel.velocloud.net",
+        # "enterprise_id": 137,
+        # "edge_id": 2839,
+        # "email": "jaylor@titanamerica.com",
+        # "phone": "540-512-7663",
+        # "name": "Jennifer Aylor"
+
+    },
+    {
+        # TA VA - Troutville (RCC - Railcar)
+        "serial": 'VC05200043210',
+        "host": "mettel.velocloud.net",
+        "enterprise_id": 137,
+        "edge_id": 2969,
+        "email": "gludwick@titanamerica.com",
+        "phone": "540-765-3145",
+        "name": "Greg Ludwick"
+
+    },
+
 ]
