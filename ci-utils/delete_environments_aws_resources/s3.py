84edd937f5e6eaa54d2fc50a3822b32ac2bc0d5b
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-11-11 14:04:50 +0100

    fix(Name of null directory)

--- ci-utils/delete_environments_aws_resources/s3.py
+++ ci-utils/delete_environments_aws_resources/s3.py
@@ -8,10 +8,14 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-FNULL = open('dev/null', 'w')
+FNULL = open('/dev/null', 'w')
 
 
 class S3Buckets:
     def delete_s3buckets(self, environment):
         logging.info("Checking if there are buckets related to the {} environment".format(environment))
 
+    def _check_s3buckets_exists(self, environment):
+        _s3_buckets_list = [f'terraform-{environment}-dev-resources.tfstate', f'terraform-{environment}-ecs-services.tfstate']
+        for element in _s3_buckets_list:
+            logging.info("Checking if s3 bucket with name {} exists".format(element))
