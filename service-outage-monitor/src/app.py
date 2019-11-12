de7ba159dff78d105a6236217e34106b1d0588f0
Daniel Fernández <daniel.fernandez@intelygenz.com>
2019-11-12 15:33:24 +0100

    Start scheduling outage-detection job on service-outage-monitor startup

--- service-outage-monitor/src/app.py
+++ service-outage-monitor/src/app.py
@@ -1,5 +1,9 @@
 import asyncio
+from application.actions.service_outage_detector import ServiceOutageDetector
 from application.actions.service_outage_monitor import ServiceOutageMonitor
+from application.repositories.template_management import TemplateRenderer
+from application.repositories.edge_repository import EdgeRepository
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pytz import timezone
 
@@ -8,7 +12,7 @@ from igz.packages.Logger.logger_client import LoggerClient
 from igz.packages.eventbus.eventbus import EventBus
 from igz.packages.nats.clients import NATSClient
 from igz.packages.server.api import QuartServer
-from application.repositories.template_management import TemplateRenderer
+
 
 
 class Container:
@@ -19,6 +23,9 @@ class Container:
         self._scheduler = AsyncIOScheduler(timezone=timezone('US/Eastern'))
         self._server = QuartServer(config)
 
+        self._online_edge_repository = EdgeRepository(root_key='ONLINE_EDGES')
+        self._quarantine_edge_repository = EdgeRepository(root_key='EDGES_QUARANTINE')
+
         self._publisher = NATSClient(config, logger=self._logger)
         self._event_bus = EventBus(logger=self._logger)
         self._event_bus.set_producer(self._publisher)
@@ -26,11 +33,17 @@ class Container:
 
         self._service_outage_monitor = ServiceOutageMonitor(self._event_bus, self._logger, self._scheduler,
                                                             config, self._template_renderer)
+        self._service_outage_detector = ServiceOutageDetector(self._event_bus, self._logger, self._scheduler,
+                                                              self._online_edge_repository,
+                                                              self._quarantine_edge_repository,
+                                                              config)
+
 
     async def _start(self):
         await self._event_bus.connect()
 
         # await self._service_outage_monitor.start_service_outage_monitor_job(exec_on_start=False)
+        await self._service_outage_detector.start_service_outage_detector_job(exec_on_start=True)
 
         self._scheduler.start()
 
