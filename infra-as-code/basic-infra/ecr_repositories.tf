7e4100a0fdffd40715aabc42da2e7a2af2c535de
Xoán Mallón <xoan.mallon@intelygenz.com>
2019-11-15 14:30:25 +0100

    Update ecr repositories

--- infra-as-code/basic-infra/ecr_repositories.tf
+++ infra-as-code/basic-infra/ecr_repositories.tf
@@ -1,47 +1,47 @@
 resource "aws_ecr_repository" "automation-nats-server" {
-  name = "automation-nats-server-2"
+  name = "automation-nats-server"
 }
 
 resource "aws_ecr_repository" "automation-velocloud-orchestrator" {
-  name = "automation-velocloud-orchestrator-2"
+  name = "automation-velocloud-orchestrator"
 }
 
 resource "aws_ecr_repository" "automation-last-contact-report" {
-  name = "automation-last-contact-report-2"
+  name = "automation-last-contact-report"
 }
 
 resource "aws_ecr_repository" "automation-service-affecting-monitor" {
-  name = "automation-service-affecting-monitor-2"
+  name = "automation-service-affecting-monitor"
 }
 
 resource "aws_ecr_repository" "automation-service-outage-monitor" {
-  name = "automation-service-outage-monitor-2"
+  name = "automation-service-outage-monitor"
 }
 
 resource "aws_ecr_repository" "automation-service-outage-triage" {
-  name = "automation-service-outage-triage-2"
+  name = "automation-service-outage-triage"
 }
 
 resource "aws_ecr_repository" "automation-velocloud-bridge" {
-  name = "automation-velocloud-bridge-2"
+  name = "automation-velocloud-bridge"
 }
 
 resource "aws_ecr_repository" "automation-bruin-bridge" {
-  name = "automation-bruin-bridge-2"
+  name = "automation-bruin-bridge"
 }
 
 resource "aws_ecr_repository" "automation-t7-bridge" {
-  name = "automation-t7-bridge-2"
+  name = "automation-t7-bridge"
 }
 
 resource "aws_ecr_repository" "automation-notifier" {
-  name = "automation-notifier-2"
+  name = "automation-notifier"
 }
 
 resource "aws_ecr_repository" "automation-metrics-prometheus" {
-  name = "automation-metrics-dashboard/prometheus-2"
+  name = "automation-metrics-dashboard/prometheus"
 }
 
 resource "aws_ecr_repository" "automation-metrics-grafana" {
-  name = "automation-metrics-dashboard/grafana-2"
+  name = "automation-metrics-dashboard/grafana"
 }
