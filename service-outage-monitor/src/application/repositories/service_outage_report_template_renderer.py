d2d21814d4dd43f42bf79512679aaafa39c732ff
David Carrasco <david.carrasco@intelygenz.com>
2019-11-19 11:01:52 +0100

    Templates, repository and test for Service Outage Report

---
+++ service-outage-monitor/src/application/repositories/service_outage_report_template_renderer.py
@@ -0,0 +1,76 @@
+from collections import OrderedDict
+
+import jinja2
+from shortuuid import uuid
+from datetime import datetime, timedelta
+import base64
+import pandas as pd
+
+from collections import OrderedDict
+
+import jinja2
+from shortuuid import uuid
+from datetime import datetime, timedelta
+import base64
+import pandas as pd
+
+
+class ServiceOutageReportTemplateRenderer:
+
+    def __init__(self, config):
+        self._config = config
+
+    def _compose_email_object(self, edges_to_report, **kwargs):
+        template_vars = {}
+        templateLoader = jinja2.FileSystemLoader(searchpath=".")
+        template = "src/templates/{}".format(kwargs.get("template", "report_mail_template.html"))
+        logo = "src/templates/images/{}".format(kwargs.get("logo", "logo.png"))
+        header = "src/templates/images/{}".format(kwargs.get("header", "header.jpg"))
+        csv = "src/templates/{}".format(kwargs.get("csv", "report_mail_template.csv"))
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        templ = templateEnv.get_template(template)
+
+        template_vars["__EDGE_COUNT__"] = str(len(edges_to_report))
+        template_vars["__TIME_REPORT__"] = kwargs.get("time_report", None)
+        template_vars["__FIELDS__"] = kwargs.get("fields", list(edges_to_report[0].keys()))
+        template_vars["__FIELDS_REL__"] = {field: field_related for field, field_related in
+                                           zip(template_vars["__FIELDS__"], kwargs.get("fields_edge"))}
+        list_rows = []
+        for idx, edge in enumerate(edges_to_report):
+            list_rows.append({
+                "type": "even" if idx % 2 == 0 else "odd",
+                **edge
+            })
+            list_rows[-1]["links"] = "<br/>".join(f"<a href=\"{i}\">{i}</a>" for i in
+                                                  list_rows[-1]["links"].split(" - "))
+        template_vars["list_row"] = list_rows
+        edges_dataframe = pd.DataFrame(edges_to_report)
+        edges_dataframe.index.name = 'idx'
+        edges_dataframe.to_csv(csv, index=False)
+        email_html = templ.render(**template_vars)
+
+        return {
+            'request_id': uuid(),
+            'email_data': {
+                'subject': f'Service Outage Report ({datetime.now().strftime("%Y-%m-%d")})',
+                'recipient': self._config.MONITOR_CONFIG["recipient"],
+                'text': 'this is the accessible text for the email',
+                'html': email_html,
+                'images': [
+                    {
+                        'name': 'logo',
+                        'data': base64.b64encode(open(logo, 'rb').read()).decode('utf-8')
+                    },
+                    {
+                        'name': 'header',
+                        'data': base64.b64encode(open(header, 'rb').read()).decode('utf-8')
+                    },
+                ],
+                'attachments': [
+                    {
+                        'name': csv,
+                        'data': base64.b64encode(open(csv, 'rb').read()).decode('utf-8')
+                    }
+                ]
+            }
+        }
---
+++ service-outage-monitor/src/tests/repositories/service_outage_report_template_renderer_test.py
@@ -0,0 +1,48 @@
+from unittest.mock import Mock
+from config import testconfig as config
+from application.repositories.service_outage_report_template_renderer import ServiceOutageReportTemplateRenderer
+from asynctest import CoroutineMock
+import base64
+from config import testconfig
+
+
+class TestServiceOutageReportTemplateRenderer:
+
+    def instantiation_test(self):
+        test_repo = ServiceOutageReportTemplateRenderer(config)
+        assert test_repo._config == config
+
+    def compose_email_object_test(self):
+        config = testconfig
+        template_renderer = ServiceOutageReportTemplateRenderer(config)
+        edges_to_report = [{
+            "detection_time": "timestamp",
+            "serial_number": "safasfasdf",
+            "enterprise": "ajsdkfjhsda",
+            "links": "http://asdasfsdf.es - http://asdlkjfhasdlkjfhweq.com - http://asdfsdf.com",
+            "tickets": "No"
+        }]
+        fields = ["Date of detection", "Serial Number", "Company", "LINKS", "Has ticket created?"]
+        fields_edge = ["detection_time", "serial_number", "enterprise", "links"]
+        email = template_renderer._compose_email_object(edges_to_report, fields=fields, fields_edge=fields_edge)
+
+        assert 'Service Outage Report' in email["email_data"]["subject"]
+        assert config.MONITOR_CONFIG["recipient"] in email["email_data"]["recipient"]
+        assert "<!DOCTYPE html" in email["email_data"]["html"]
+
+    def compose_email_table_fields_and_values_check_test(self):
+        config = testconfig
+        template_renderer = ServiceOutageReportTemplateRenderer(config)
+        edges_to_report = [{
+            "detection_time": "timestamp",
+            "serial_number": "safasfasdf",
+            "enterprise": "ajsdkfjhsda",
+            "links": "http://asdasfsdf.es - http://asdlkjfhasdlkjfhweq.com - http://asdfsdf.com",
+            "tickets": "No"
+        }]
+        fields = ["Date of detection", "Serial Number", "Company", "LINKS", "Has ticket created?"]
+        fields_edge = ["detection_time", "serial_number", "enterprise", "links", "tickets"]
+        email = template_renderer._compose_email_object(edges_to_report, fields=fields, fields_edge=fields_edge)
+
+        assert all(f"{i}</th>" in email["email_data"]["html"] for i in fields) is True
+        assert all((f"{ii}</td >" in email["email_data"]["html"] for ii in i) for i in edges_to_report) is True
