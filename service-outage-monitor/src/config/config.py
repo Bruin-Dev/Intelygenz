926dde22830483f75abf87b19ba6ad1a75e014d4
brandon.samudio <brandon.samudio@intelygenz.com>
2020-03-05 17:26:29 -0800

    increase outage_reporter time

--- service-outage-monitor/src/config/config.py
+++ service-outage-monitor/src/config/config.py
@@ -28,7 +28,7 @@ MONITOR_CONFIG = {
     'jobs_intervals': {
         'outage_detector': 60 * 40,
         'outage_monitor': 60 * 3,
-        'outage_reporter': 60 * 210,
+        'outage_reporter': 60 * 240,
         'quarantine': quarantine_time,
     },
     'quarantine_key_ttl': quarantine_time + 60 * 5,
