8e6a4b9795b66295bf338e3b51e614c6430a0b91
Juan Carlos Gómez Sánchez <juancarlos.gomez@intelygenz.com>
2020-07-01 11:31:45 +0200

    [service-dispatch-monitor] CTS templates for sms and notes

--- service-dispatch-monitor/src/application/templates/cts/cts_dispatch_confirmed.py
+++ service-dispatch-monitor/src/application/templates/cts/cts_dispatch_confirmed.py
@@ -27,6 +27,17 @@ def cts_get_dispatch_confirmed_sms_note(body):
     )
 
 
+CTS_DISPATCH_CONFIRMED_SMS_TECH = """#*Automation Engine*#
+Dispatch confirmation SMS tech sent to {phone_number}
+"""
+
+
+def cts_get_dispatch_confirmed_sms_tech_note(body):
+    return CTS_DISPATCH_CONFIRMED_SMS_TECH.format(
+        phone_number=body.get('phone_number')
+    )
+
+
 CTS_TECH_12_HOURS_BEFORE_SMS_NOTE = """#*Automation Engine*#
 Dispatch 12h prior reminder SMS sent to {phone_number}
 """
@@ -38,6 +49,17 @@ def cts_get_tech_12_hours_before_sms_note(body):
     )
 
 
+CTS_TECH_12_HOURS_BEFORE_SMS_TECH_NOTE = """#*Automation Engine*#
+Dispatch 12h prior reminder tech SMS sent to {phone_number}
+"""
+
+
+def cts_get_tech_12_hours_before_sms_tech_note(body):
+    return CTS_TECH_12_HOURS_BEFORE_SMS_TECH_NOTE.format(
+        phone_number=body.get('phone_number')
+    )
+
+
 CTS_TECH_2_HOURS_BEFORE_SMS_NOTE = """#*Automation Engine*#
 Dispatch 2h prior reminder SMS sent to {phone_number}
 """
@@ -45,5 +67,16 @@ Dispatch 2h prior reminder SMS sent to {phone_number}
 
 def cts_get_tech_2_hours_before_sms_note(body):
     return CTS_TECH_2_HOURS_BEFORE_SMS_NOTE.format(
+        phone_number=body.get('phone_number')
+    )
+
+
+CTS_TECH_2_HOURS_BEFORE_SMS_TECH_NOTE = """#*Automation Engine*#
+Dispatch 2h prior reminder tech SMS sent to {phone_number}
+"""
+
+
+def cts_get_tech_2_hours_before_sms_tech_note(body):
+    return CTS_TECH_2_HOURS_BEFORE_SMS_TECH_NOTE.format(
         phone_number=body.get('phone_number')
     )
--- service-dispatch-monitor/src/application/templates/cts/sms/dispatch_confirmed.py
+++ service-dispatch-monitor/src/application/templates/cts/sms/dispatch_confirmed.py
@@ -10,6 +10,18 @@ def cts_get_dispatch_confirmed_sms(body):
     )
 
 
+CTS_SMS_DISPATCH_CONFIRMED_TECH = """This is an automated message from MetTel customer support.
+
+You have been confirmed for a dispatch on {date_of_dispatch}.
+"""
+
+
+def cts_get_dispatch_confirmed_sms_tech(body):
+    return CTS_SMS_DISPATCH_CONFIRMED_TECH.format(
+        date_of_dispatch=body.get('date_of_dispatch')
+    )
+
+
 CTS_SMS_DISPATCH_TECH_12_HOURS_BEFORE_SMS = """This is an automated message from MetTel customer support.
 
 A field engineer will arrive in 12 hours, {date_of_dispatch}, at your location.
@@ -24,6 +36,18 @@ def cts_get_tech_12_hours_before_sms(body):
     )
 
 
+CTS_SMS_DISPATCH_TECH_12_HOURS_BEFORE_SMS_TECH = """This is an automated message from MetTel customer support.
+
+You have a dispatch coming up in 12 hours, {date_of_dispatch}.
+"""
+
+
+def cts_get_tech_12_hours_before_sms_tech(body):
+    return CTS_SMS_DISPATCH_TECH_12_HOURS_BEFORE_SMS_TECH.format(
+        date_of_dispatch=body.get('date_of_dispatch')
+    )
+
+
 CTS_SMS_DISPATCH_TECH_2_HOURS_BEFORE_SMS = """This is an automated message from MetTel customer support.
 
 A field engineer will arrive in 2 hours, {date_of_dispatch}, at your location.
@@ -34,5 +58,17 @@ You will receive a text message at this number when they have arrived.
 
 def cts_get_tech_2_hours_before_sms(body):
     return CTS_SMS_DISPATCH_TECH_2_HOURS_BEFORE_SMS.format(
+        date_of_dispatch=body.get('date_of_dispatch')
+    )
+
+
+CTS_SMS_DISPATCH_TECH_2_HOURS_BEFORE_SMS_TECH = """This is an automated message from MetTel customer support.
+
+You have a dispatch coming up in 2 hours, {date_of_dispatch}.
+"""
+
+
+def cts_get_tech_2_hours_before_sms_tech(body):
+    return CTS_SMS_DISPATCH_TECH_2_HOURS_BEFORE_SMS_TECH.format(
         date_of_dispatch=body.get('date_of_dispatch')
     )
