8780fc6fe63cf235af8f87d79177167d75e983c8
Xoan Mallon Moure <xoan.mallon@intelygenz.com>
2020-07-09 14:46:41 +0200

    [dispatch-portal-frontend] Fix log format used in dispatch-portal-frontend-nginx

--- ci-utils/papertrail-provisioning/config.py
+++ ci-utils/papertrail-provisioning/config.py
@@ -58,7 +58,7 @@ PAPERTRAIL_PROVISIONING = {
                     "search_name": f"[lumin-billing-report] - logs"
                 },
                 {
-                    "query": f"[NINGX LOGS][{BUILD_NUMBER}][{ENVIRONMENT_ID}]",
+                    "query": f"NGINX AND {BUILD_NUMBER} AND {ENVIRONMENT_ID}",
                     "search_name": f"[nginx] - logs"
                 },
                 {
