c1aab3432d987d6973bba5dfa4fe0efb44a91920
brandon.samudio <brandon.samudio@intelygenz.com>
2021-03-19 09:48:05 -0700

    [service-affecting] increase jitter threshold

--- service-affecting-monitor/src/config/config.py
+++ service-affecting-monitor/src/config/config.py
@@ -24,7 +24,7 @@ MONITOR_CONFIG = {
     'timezone': 'US/Eastern',
     "latency": 120,
     "packet_loss": 8,
-    "jitter": 30,
+    "jitter": 50,
     "bandwidth_percentage": 90,
     'monitoring_minutes_interval': 10,
     "monitoring_minutes_per_trouble": {
