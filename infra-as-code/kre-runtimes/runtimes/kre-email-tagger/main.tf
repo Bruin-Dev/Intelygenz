a8d7ad164a8149e4662186ac20d8655163addac7
Xoan Mallon Moure <xoan.mallon@intelygenz.com>
2021-04-05 13:49:02 +0200

    [infra-as-code]: Update kre infrastructure variables in module

--- infra-as-code/kre-runtimes/runtimes/kre-email-tagger/main.tf
+++ infra-as-code/kre-runtimes/runtimes/kre-email-tagger/main.tf
@@ -2,4 +2,5 @@ module "kre-tnba-runtime" {
   source = "../../modules/kre-runtime"
 
   RUNTIME_NAME = "kre-email-tagger"
+  CURRENT_ENVIRONMENT = var.CURRENT_ENVIRONMENT
 }
--- infra-as-code/kre-runtimes/runtimes/kre-tnba-runtime/main.tf
+++ infra-as-code/kre-runtimes/runtimes/kre-tnba-runtime/main.tf
@@ -2,4 +2,5 @@ module "kre-tnba-runtime" {
   source = "../../modules/kre-runtime"
 
   RUNTIME_NAME = "kre-tnba"
+  CURRENT_ENVIRONMENT = var.CURRENT_ENVIRONMENT
 }
