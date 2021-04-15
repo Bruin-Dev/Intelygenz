74c3e4109474594e4ed95a2725ac8d64139de9de
Daniel Fernández <daniel.fernandez@intelygenz.com>
2021-04-15 12:45:48 +0200

    [service-affecting-monitor] Fix naming in Prometheus' counter for Affecting tickets created

--- service-affecting-monitor/src/application/repositories/metrics_repository.py
+++ service-affecting-monitor/src/application/repositories/metrics_repository.py
@@ -2,7 +2,7 @@ from prometheus_client import Counter
 
 
 class MetricsRepository:  # pragma: no cover
-    _tickets_created = Counter('outage_ticket_counter', 'Tickets created')
+    _tickets_created = Counter('affecting_ticket_counter', 'Tickets created')
     _tickets_reopened = Counter('reopened_counter', 'Tickets reopened')
 
     def increment_tickets_created(self):
