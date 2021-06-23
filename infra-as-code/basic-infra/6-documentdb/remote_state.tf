fe27dd5641af1f8fd0b6158db79434ced621f0cc
Angel Costales <angel.costales@intelygenz.com>
2021-06-23 10:45:54 +0200

    [infra-as-code] import documentdb for ticket-collector

---
+++ infra-as-code/basic-infra/6-documentdb/remote_state.tf
@@ -0,0 +1,12 @@
+# EKS Backend
+terraform {
+  backend "s3" {
+    bucket = "automation-infrastructure"
+    region = "us-east-1"
+    key    = "mettel-automation-docdb-ticket-collector.tfstate"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
