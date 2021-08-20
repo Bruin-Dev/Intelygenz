033635f60548f6e030a8ba681c290e3cfed65956
Angel Costales <angel.costales@intelygenz.com>
2021-08-20 14:03:38 +0200

    [infra-as-code] Delete all related files from deleted data-collector microservice

--- infra-as-code/data-collector/output.tf
+++
@@ -1,4 +0,0 @@
-output "api_gateway_endpoint_data_collector" {
-  description = "API Gateway endpoint for call data-collector lambda"
-  value       = aws_apigatewayv2_api.data-collector-api-gateway.api_endpoint
-}
