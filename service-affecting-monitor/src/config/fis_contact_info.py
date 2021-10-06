14e85c22053aa5f521551d3d4015d944e5f4b92c
Daniel Fernández <daniel.fernandez@intelygenz.com>
2021-10-06 11:51:01 +0200

    feat(service-affecting-monitor): add FIS edges to monitoring

---
+++ service-affecting-monitor/src/config/fis_contact_info.py
@@ -0,0 +1,347 @@
+﻿devices_by_id = [
+    # Client ID: 87955
+    {
+        "serial": "VC05400016457",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 15,
+        "edge_id": 20,
+        "contacts": {
+            "ticket": {
+                "name": "Fis Fis",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan@fisglobal.com"
+            },
+            "site": {
+                "name": "Fis Fis",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "VC05400016672",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 15,
+        "edge_id": 19,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    # Client ID: 89180
+    {
+        "serial": "4XJKPK2",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 72,
+        "edge_id": 652,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "VC05400037229",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 72,
+        "edge_id": 529,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "VC05400037367",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 72,
+        "edge_id": 530,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "VC05400028376",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 72,
+        "edge_id": 527,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "VC05400028245",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 72,
+        "edge_id": 532,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "VC05400037546",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 72,
+        "edge_id": 528,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "VC05400028108",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 72,
+        "edge_id": 531,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "VC05400035753",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 72,
+        "edge_id": 525,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "VC05400027806",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 72,
+        "edge_id": 534,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "VC05400037133",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 72,
+        "edge_id": 535,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "VC05400037509",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 72,
+        "edge_id": 526,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "VC05400037253",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 72,
+        "edge_id": 533,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    # Client ID: 89342
+    {
+        "serial": "VC05400016457",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 79,
+        "edge_id": 20,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Help Desk",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "VC05400016672",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 79,
+        "edge_id": 19,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Help Desk",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "8125PK2",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 79,
+        "edge_id": 635,
+        "contacts": {
+            "ticket": {
+                "name": "Karen rose Talenjale",
+                "phone": "6028578161",
+                "email": "karen.rose.d.borromeo@fisglobal.com"
+            },
+            "site": {
+                "name": "Karen rose Talenjale",
+                "phone": "6028578161",
+                "email": "karen.rose.d.borromeo@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "GM15PK2",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 79,
+        "edge_id": 634,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    },
+    {
+        "serial": "7M15PK2",
+        "host": "metvco04.mettel.net",
+        "enterprise_id": 79,
+        "edge_id": 633,
+        "contacts": {
+            "ticket": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            },
+            "site": {
+                "name": "Pronet spoc Support team",
+                "phone": "6023875757",
+                "email": "az_phx_team-sdwan-support@fisglobal.com"
+            }
+        }
+    }
+]
