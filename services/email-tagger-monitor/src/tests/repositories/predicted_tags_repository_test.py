215e8257c8da74dbcbf31459219d485dc67cfdaa
Marc Vivancos <marc.vivancos@intelygenz.com>
2021-11-16 15:43:00 +0100

    feat(email-tagger-monitor): add predicted tags repository to save predictions

---
+++ services/email-tagger-monitor/src/tests/repositories/predicted_tags_repository_test.py
@@ -0,0 +1,57 @@
+from unittest.mock import Mock
+
+from application.repositories.predicted_tags_repository import PredictedTagsRepository
+from config import testconfig
+
+
+class TestPredictedTagsRepository:
+    def instance_test(self):
+        logger = Mock()
+        config = testconfig
+        notifications_repository = Mock()
+        storage_repository = Mock()
+
+        predicted_tags_repository = PredictedTagsRepository(logger, config, notifications_repository,
+                                                            storage_repository)
+
+        assert predicted_tags_repository._logger is logger
+        assert predicted_tags_repository._config is config
+        assert predicted_tags_repository._notifications_repository is notifications_repository
+        assert predicted_tags_repository._storage_repository is storage_repository
+
+    def get_pending_tags_ok_test(self, logger, notifications_repository, storage_repository):
+        storage_repository.find_all = Mock(return_value=[])
+        predicted_tags_repository = PredictedTagsRepository(testconfig, logger, notifications_repository,
+                                                            storage_repository)
+
+        actual = predicted_tags_repository.get_pending_tags()
+
+        storage_repository.find_all.assert_called_once()
+        assert actual == []
+
+    def save_new_tag_ok_test(self, logger, notifications_repository, storage_repository):
+        storage_repository.save = Mock()
+        predicted_tags_repository = PredictedTagsRepository(logger, testconfig, notifications_repository,
+                                                            storage_repository)
+
+        expected_tag = "tag_email_123456"
+        tag_data = {
+            "email_id": "123456",
+            "tag_id": "123"
+        }
+        response = predicted_tags_repository.save_new_tag(**tag_data)
+
+        storage_repository.save.assert_called_once_with(expected_tag, tag_data)
+        assert response is None
+
+    def mark_complete_ok_test(self, logger, notifications_repository, storage_repository):
+        storage_repository.remove = Mock()
+        predicted_tags_repository = PredictedTagsRepository(logger, testconfig, notifications_repository,
+                                                            storage_repository)
+
+        email_id = "123456"
+        expected_tag_email_id = "tag_email_123456"
+        response = predicted_tags_repository.mark_complete(email_id)
+
+        storage_repository.remove.assert_called_once_with(expected_tag_email_id)
+        assert response is None
