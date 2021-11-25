84007e4e2d198716a24d1e616a65c998fb33d869
Marc Vivancos <marc.vivancos@intelygenz.com>
2021-11-25 10:41:30 +0100

    feat(repair-tickets-monitor): add verification methods bruin repository

--- services/repair-tickets-monitor/src/application/repositories/bruin_repository.py
+++ services/repair-tickets-monitor/src/application/repositories/bruin_repository.py
@@ -3,6 +3,7 @@ from shortuuid import uuid
 from tenacity import retry, wait_exponential, stop_after_delay
 
 from application.repositories import nats_error_response
+from typing import Any, Dict, List
 
 
 class BruinRepository:
@@ -75,3 +76,196 @@ class BruinRepository:
             return await get_single_ticket_basic_info()
         except Exception as e:
             self._logger.error(f"Error getting ticket {ticket_id} from Bruin: {e}")
+
+    async def verify_service_number_information(self, client_id, service_number):
+        @retry(
+            wait=wait_exponential(
+                multiplier=self._config.NATS_CONFIG['multiplier'],
+                min=self._config.NATS_CONFIG['min']
+            ),
+            stop=stop_after_delay(self._config.NATS_CONFIG['stop_delay'])
+        )
+        async def verify_service_number_information():
+            err_msg = None
+            self._logger.info(f'Getting inventory "{client_id}" and service number {service_number}')
+            request_msg = {
+                "request_id": uuid(),
+                "body": {
+                    "client_id": client_id,
+                    "service_number": service_number,
+                    "status": "A",
+                }
+            }
+            try:
+                response = await self._event_bus.rpc_request(
+                    "bruin.customer.get.info",
+                    request_msg,
+                    timeout=self._timeout
+                )
+
+            except Exception as err:
+                err_msg = (
+                    f'An error occurred when getting service number info from Bruin, '
+                    f'for ticket_id "{client_id}" -> {err}'
+                )
+                response = nats_error_response
+            else:
+                response_body = response['body']
+                response_status = response['status']
+
+                if response_status not in range(200, 300):
+                    err_msg = (
+                        f'Error getting service number info for {service_number} in '
+                        f'{self._config.ENVIRONMENT.upper()} environment: '
+                        f'Error {response_status} - {response_body}'
+                    )
+                else:
+                    response['body'] = {
+                        'client_id': client_id,
+                        'site_id': response['body'].get('site_id'),
+                        'service_number': service_number,
+                    }
+
+            if err_msg:
+                self._logger.error(err_msg)
+                await self._notifications_repository.send_slack_message(err_msg)
+            else:
+                self._logger.info(f'Service number info {service_number} retrieved from Bruin')
+
+            return response
+
+        try:
+            return await verify_service_number_information()
+        except Exception as e:
+            self._logger.error(f"Error getting service number info {service_number} from Bruin: {e}")
+
+    async def get_ticket_details(self, ticket_id: int):
+        @retry(
+            wait=wait_exponential(
+                multiplier=self._config.NATS_CONFIG['multiplier'],
+                min=self._config.NATS_CONFIG['min']
+            ),
+            stop=stop_after_delay(self._config.NATS_CONFIG['stop_delay'])
+        )
+        async def get_ticket_details():
+            err_msg = None
+
+            request = {
+                'request_id': uuid(),
+                'body': {
+                    'ticket_id': ticket_id
+                },
+            }
+
+            try:
+                self._logger.info(f'Getting details of ticket {ticket_id} from Bruin...')
+                response = await self._event_bus.rpc_request("bruin.ticket.details.request", request, timeout=15)
+            except Exception as e:
+                err_msg = f'An error occurred when requesting ticket details ' \
+                          f'from Bruin API for ticket {ticket_id} -> {e}'
+                response = nats_error_response
+            else:
+                response_body = response['body']
+                response_status = response['status']
+
+                if response_status not in range(200, 300):
+                    err_msg = (
+                        f'Error while retrieving details of ticket {ticket_id} in '
+                        f'{self._config.MONITOR_CONFIG["environment"].upper()} environment: '
+                        f'Error {response_status} - {response_body}'
+                    )
+                else:
+                    self._logger.info(f'Got details of ticket {ticket_id} from Bruin!')
+
+            if err_msg:
+                self._logger.error(err_msg)
+                await self._notifications_repository.send_slack_message(err_msg)
+            return response
+
+        try:
+            return await get_ticket_details()
+        except Exception as e:
+            self._logger.error(f"Error getting ticket details for {ticket_id} from Bruin: {e}")
+
+    async def get_site(self, site_id):
+        # TODO add for ticket creation
+        pass
+
+    async def get_tickets_basic_info(self, client_id: str, ticket_statuses: List[str]):
+        @retry(
+            wait=wait_exponential(
+                multiplier=self._config.NATS_CONFIG['multiplier'],
+                min=self._config.NATS_CONFIG['min']
+            ),
+            stop=stop_after_delay(self._config.NATS_CONFIG['stop_delay'])
+        )
+        async def get_tickets():
+            err_msg = None
+            request = {
+                'request_id': uuid(),
+                'body': {
+                    # TODO add site id
+                    'client_id': client_id,
+                    'ticket_statuses': ticket_statuses
+                },
+            }
+
+            try:
+                self._logger.info(
+                    f'Getting all tickets with any status of {ticket_statuses},'
+                    f'and belonging to client {client_id} from Bruin...'
+                )
+                response = await self._event_bus.rpc_request("bruin.ticket.basic.request", request, timeout=90)
+            except Exception as e:
+                err_msg = (
+                    f'An error occurred when requesting tickets from Bruin API with any status of {ticket_statuses} '
+                    f'and belonging to client {client_id} -> {e}'
+                )
+                response = nats_error_response
+            else:
+                response_body = response['body']
+                response_status = response['status']
+
+                if response_status in range(200, 300):
+                    self._logger.info(
+                        f'Got all tickets with any status of {ticket_statuses}, with ticket topic '
+                        f'and belonging to client {client_id} from Bruin!'
+                    )
+
+                else:
+                    err_msg = (
+                        f'Error while retrieving tickets with any status of {ticket_statuses} '
+                        f'and belonging to client {client_id} in '
+                        f'{self._config.MONITOR_CONFIG["environment"].upper()} environment: '
+                        f'Error {response_status} - {response_body}'
+                    )
+            if err_msg:
+                self._logger.error(err_msg)
+                await self._notifications_repository.send_slack_message(err_msg)
+
+            return response
+
+        try:
+            return await get_tickets()
+        except Exception as e:
+            self._logger.error(f"Error getting tickets with client_id {client_id} from Bruin: {e}")
+
+    async def get_open_tickets_with_serial_numbers(self, client_id: str) -> Dict[str, Any]:
+        ticket_statuses = ['New', 'InProgress', 'Draft']
+
+        tickets = await self.get_tickets_basic_info(client_id, ticket_statuses)
+        for idx, ticket in enumerate(tickets):
+            ticket_id = ticket['ticketId']
+            details = await self.get_ticket_details(ticket_id)
+            serial_numbers = self._get_details_serial_numbers(details)
+            ticket[idx]['serial_numbers'] = serial_numbers
+
+        return tickets
+
+    def _get_details_serial_numbers(self, ticket_details: Dict[str, Any]) -> List[str]:
+        notes = ticket_details.get('ticketNotes')
+        unique_serial_numbers = set()
+        for note in notes:
+            note_serial_numbers = note.get('serviceNumber', [])
+            unique_serial_numbers.add(*note_serial_numbers)
+        return list(unique_serial_numbers)
--- services/repair-tickets-monitor/src/tests/repositories/bruin_repository_test.py
+++ services/repair-tickets-monitor/src/tests/repositories/bruin_repository_test.py
@@ -19,7 +19,7 @@ class TestBruinRepository:
         assert bruin_repository._notifications_repository is notifications_repository
 
     @pytest.mark.asyncio
-    async def get_single_ticket_basic_info_ok_test(
+    async def get_single_ticket_basic_info__ok_test(
             self,
             event_bus,
             bruin_repository,
@@ -47,7 +47,7 @@ class TestBruinRepository:
         assert result == response
 
     @pytest.mark.asyncio
-    async def get_single_ticket_basic_info_not_2XX_test(
+    async def get_service_number_information__not_2XX_test(
             self,
             event_bus,
             bruin_repository,
@@ -64,5 +64,50 @@ class TestBruinRepository:
 
         event_bus.rpc_request.assert_awaited()
         notifications_repository.send_slack_message.assert_awaited_once()
+
+        assert result == response
+
+    @pytest.mark.asyncio
+    async def get_service_number_information__ok_test(
+            self,
+            event_bus,
+            bruin_repository,
+            make_rpc_request,
+            make_rpc_response,
+    ):
+        client_id = '12345'
+        service_number = '1234'
+        site_id = '1234'
+        request = make_rpc_request(request_id=uuid_, client_id=client_id, service_number=service_number, status='A')
+        response_body = {'client_id': client_id, 'client_name': client_id, 'site_id': site_id}
+        response = make_rpc_response(request_id=uuid_, body=response_body, status=200)
+
+        event_bus.rpc_request.return_value = response
+        with patch('application.repositories.bruin_repository.uuid', return_value=uuid_):
+            result = await bruin_repository.get_service_number_information(client_id, service_number)
+
+        event_bus.rpc_request.assert_awaited_once_with(
+            "bruin.customer.get.info",
+            request,
+            timeout=config.MONITOR_CONFIG['nats_request_timeout']['bruin_request_seconds']
+        )
+
+        assert result == response
+
+    @pytest.mark.asyncio
+    async def get_service_number_information__non_2XX_status_test(
+            self,
+            event_bus,
+            bruin_repository,
+            make_rpc_response,
+    ):
+        client_id = '12345'
+        service_number = '1234'
+        response_body = {'error': 'error'}
+        response = make_rpc_response(request_id=uuid_, body=response_body, status=400)
+
+        event_bus.rpc_request.return_value = response
+        with patch('application.repositories.bruin_repository.uuid', return_value=uuid_):
+            result = await bruin_repository.get_service_number_information(client_id, service_number)
 
         assert result == response
