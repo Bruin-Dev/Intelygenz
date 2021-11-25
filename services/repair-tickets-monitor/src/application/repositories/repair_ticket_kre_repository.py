c59a562937987acf86a6d640cb24d87bfe1d7a07
Marc Vivancos <marc.vivancos@intelygenz.com>
2021-11-25 10:40:30 +0100

    feat(repair-tickets-monitor): add save outputs to rta kre repo

--- services/repair-tickets-monitor/src/application/repositories/repair_ticket_kre_repository.py
+++ services/repair-tickets-monitor/src/application/repositories/repair_ticket_kre_repository.py
@@ -3,6 +3,7 @@ from shortuuid import uuid
 from tenacity import retry, wait_exponential, stop_after_delay
 
 from application.repositories import nats_error_response
+from typing import Any, Dict, List
 
 
 class RepairTicketKreRepository:
@@ -23,7 +24,7 @@ class RepairTicketKreRepository:
             ),
             stop=stop_after_delay(self._config.NATS_CONFIG["stop_delay"]),
         )
-        async def get_prediction():
+        async def get_email_inference():
             err_msg = None
 
             self._logger.info(
@@ -60,10 +61,80 @@ class RepairTicketKreRepository:
             return response
 
         try:
-            return await get_prediction()
+            return await get_email_inference()
         except Exception as e:
             self._logger.error(
                 f"Error trying to get prediction from rta KRE [email_id='{email_id}']: {e}"
+            )
+
+    async def save_outputs(
+            self,
+            email_id: str,
+            service_numbers_by_site: Dict[str, List],
+            exiting_ticket_ids: List[str],
+            created_ticket_ids: List[str],
+    ):
+        site_ids = list(service_numbers_by_site.keys())
+        service_numbers = [service_number for _, service_number_list in service_numbers_by_site
+                           for service_number in service_number_list]
+
+        @retry(
+            wait=wait_exponential(
+                multiplier=self._config.NATS_CONFIG["multiplier"],
+                min=self._config.NATS_CONFIG["min"],
+            ),
+            stop=stop_after_delay(self._config.NATS_CONFIG["stop_delay"]),
+        )
+        async def save_outputs():
+            err_msg = None
+
+            self._logger.info(
+                f"Sending output data for email_id={email_id}"
+            )
+            request_body = {
+                'email_id': email_id,
+                'site_ids': site_ids,
+                'validated_service_numbers': service_numbers,
+                'existing_tickets_ids': exiting_ticket_ids,
+                'created_tickets_ids': created_ticket_ids,
+            }
+            request_msg = {
+                "request_id": uuid(),
+                "body": request_body}
+            try:
+                response = await self._event_bus.rpc_request(
+                    "rta.prediction.request", request_msg, timeout=self._timeout
+                )
+
+            except Exception as e:
+                err_msg = f"An error occurred when saving output to rta for email_id '{email_id}' -> {e}"
+                response = nats_error_response
+            else:
+                response_body = response["body"]
+                response_status = response["status"]
+
+                if response_status not in range(200, 300):
+                    err_msg = (
+                        f'Error while getting prediction for email "{email_id}" in '
+                        f"{self._config.ENVIRONMENT.upper()} environment: "
+                        f"Error {response_status} - {response_body}"
+                    )
+
+            if err_msg:
+                self._logger.error(err_msg)
+                await self._notifications_repository.send_slack_message(err_msg)
+            else:
+                self._logger.info(
+                    f"Saving Output request sent for email {email_id} to rta"
+                )
+
+            return response
+
+        try:
+            return await save_outputs()
+        except Exception as e:
+            self._logger.error(
+                f"Error trying to get save output from rta KRE [email_id='{email_id}']: {e}"
             )
 
     async def save_created_ticket_feedback(self, email_data: dict, ticket_data: dict):
