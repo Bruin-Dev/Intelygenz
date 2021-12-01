b0e4ed0eca4f5d3a9cb14446793557dbb0a39965
Marc Vivancos <marc.vivancos@intelygenz.com>
2021-12-01 18:21:21 +0100

    fix(repair-tickets-monitor): change import path of some fixtures

--- services/repair-tickets-monitor/src/tests/fixtures/bruin.py
+++ services/repair-tickets-monitor/src/tests/fixtures/bruin.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from typing import List
 import pytest
-from fixtures._helpers import bruinize_date
+from tests.fixtures._helpers import bruinize_date
 
 
 @pytest.fixture(scope='session')
