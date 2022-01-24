59f3717c88442994890b2070c7aaf74211bcaf94
Daniel Fernández <daniel.fernandez@intelygenz.com>
2022-01-24 13:54:01 +0100

    feat(dri-bridge): update config file to point to new GitLab variables

--- services/dri-bridge/src/config/config.py
+++ services/dri-bridge/src/config/config.py
@@ -42,13 +42,13 @@ NATS_CONFIG = {
 }
 
 DRI_CONFIG = {
-    'base_url': os.environ["DRI_BASE_URL"],
-    'email_acc': os.environ["DRI_ACC_EMAIL"],
-    'email_password': os.environ["DRI_ACC_PASSWORD"],
-    'redis_save_ttl': 300
-    }
+    'base_url': os.environ["BASE_URL"],
+    'email_acc': os.environ["USERNAME"],
+    'email_password': os.environ["PASSWORD"],
+    'redis_save_ttl': int(os.environ["DRI_DATA_REDIS_TTL"]),
+}
 
-ENVIRONMENT_NAME = os.getenv('ENVIRONMENT_NAME')
+ENVIRONMENT_NAME = os.environ['ENVIRONMENT_NAME']
 
 LOG_CONFIG = {
     'name': 'dri-bridge',
@@ -56,10 +56,10 @@ LOG_CONFIG = {
     'stream_handler': logging.StreamHandler(sys.stdout),
     'format': f'%(asctime)s: {ENVIRONMENT_NAME}: %(hostname)s: %(module)s::%(lineno)d %(levelname)s: %(message)s',
     'papertrail': {
-        'active': True if os.getenv('PAPERTRAIL_ACTIVE') == "true" else False,
+        'active': os.environ['PAPERTRAIL_ACTIVE'] == "true",
         'prefix': os.getenv('PAPERTRAIL_PREFIX', f'{ENVIRONMENT_NAME}-dri-bridge'),
-        'host': os.getenv('PAPERTRAIL_HOST'),
-        'port': int(os.getenv('PAPERTRAIL_PORT'))
+        'host': os.environ['PAPERTRAIL_HOST'],
+        'port': int(os.environ['PAPERTRAIL_PORT'])
     },
 }
 
