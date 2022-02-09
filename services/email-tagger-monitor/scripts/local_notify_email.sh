09b3be88dbe208b459e93e26d0143c5379da8faf
David García <david.garciaguerrero@intelygenz.com>
2022-02-09 16:14:43 +0100

    Add some print to trace the local_notify_email.sh script

--- services/email-tagger-monitor/scripts/local_notify_email.sh
+++ services/email-tagger-monitor/scripts/local_notify_email.sh
@@ -5,11 +5,14 @@ JSON_FILE=$1
 
 # export REQUEST_API_KEY="vcY5L1lfsQEHcUolkd928FU2tAU2aB2NCmutMlV77bA="
 export REQUEST_API_KEY=""
-export SIGNATURE_SECRET_KEY="hxCr1/RWNOaHEKvX9FY05ocQq9f0VjTmhxCr1/RWNOZw4U4x5nXsFA=="
+export SIGNATURE_SECRET_KEY="secret"
 export EMAIL_WEBHOOK_URL="http://localhost:5055/api/email-tagger-webhook/email"
 
+echo "Runing randomize..."
 PAYLOAD=$(cat $JSON_FILE | python scripts/randomize.py)
+echo  "Runing signature..."
 SIGNATURE=$(echo $PAYLOAD | python scripts/signature.py)
+echo "Scripts ran"
 
 # Uncomment to enable API KEY header
 # API_KEY_HEADER="-H \"api-key: ${REQUEST_API_KEY}\""
@@ -23,4 +26,3 @@ curl -i -XPOST \
   $API_KEY_HEADER \
   -H "x-bruin-webhook-signature: ${SIGNATURE}" \
   --data "${PAYLOAD}"  ${EMAIL_WEBHOOK_URL}
-
