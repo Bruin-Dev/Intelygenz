a101edcdaaffae93e8bc9fde1168c8d2da09e6b2
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2022-02-10 17:48:30 -0800

    perf(ticket-collector): remove performance profiling since the root cause is now fixed (missing MongoDB index)

--- services/ticket-collector/src/usecases/tickets.py
+++ services/ticket-collector/src/usecases/tickets.py
@@ -20,30 +20,6 @@ class TicketUseCase:
         self.config = config
         self.logger = logger
         self.semaphore = asyncio.BoundedSemaphore(self.config['concurrent_days'])
-        self.perf_counters = {}
-
-    def _start_perf_counter(self, counter):
-        self.perf_counters[counter] = time.perf_counter()
-
-    def _stop_perf_counter(self, counter):
-        elapsed_time = time.perf_counter() - self.perf_counters[counter]
-        self.perf_counters.pop(counter)
-        self.logger.debug(f'{counter} took {self._humanize_time(elapsed_time)}')
-
-    @staticmethod
-    def _humanize_time(t):
-        if t < 1:
-            unit = 'ms'
-            t = t * 1000
-        elif t < 60:
-            unit = 's'
-        elif t < 3600:
-            unit = 'm'
-            t = t / 60
-        else:
-            unit = 'h'
-            t = t / 3600
-        return f'{round(t, 2)}{unit}'
 
     @staticmethod
     def date_range(start_date: date, end_date: date) -> List[date]:
@@ -101,42 +77,28 @@ class TicketUseCase:
             start = _date.strftime('%Y-%m-%dT00:00:00Z')
             end = _date.strftime('%Y-%m-%dT23:59:59Z')
 
-            self._start_perf_counter('get_tickets_from_bruin')
             tickets = await self.bruin_repository.request_tickets_by_date_range(start=start, end=end)
-            self._stop_perf_counter('get_tickets_from_bruin')
             update = self.should_update(_date=_date, today=today)
 
             for ticket in tickets:
                 ticket_id = ticket['ticketID']
-                self._start_perf_counter('get_ticket_from_mongodb')
                 ticket_on_mongo = self.tickets_repository.get_ticket_by_id(ticket_id=ticket_id)
-                self._stop_perf_counter('get_ticket_from_mongodb')
 
                 if update:
-                    self._start_perf_counter('delete_ticket_from_mongodb')
                     self.tickets_repository.delete_ticket(ticket_id=ticket_id)
-                    self._stop_perf_counter('delete_ticket_from_mongodb')
 
                 if update or not ticket_on_mongo:
-                    self._start_perf_counter('save_ticket_on_mongodb')
                     self.tickets_repository.save_ticket(ticket=ticket)
-                    self._stop_perf_counter('save_ticket_on_mongodb')
 
                 await self.save_ticket_events(ticket_id)
 
             self.logger.info(f'Finished getting tickets from {_date}')
 
     async def save_ticket_events(self, ticket_id: int) -> None:
-        self._start_perf_counter('get_ticket_events_from_bruin')
         events = await self.bruin_repository.request_ticket_events(ticket_id=ticket_id)
-        self._stop_perf_counter('get_ticket_events_from_bruin')
 
         if events:
-            self._start_perf_counter('save_ticket_events_on_mongodb')
             self.tickets_repository.save_events(ticket_id=ticket_id, events=events)
-            self._stop_perf_counter('save_ticket_events_on_mongodb')
         else:
             self.logger.info(f"We don't have access to ticket {ticket_id}")
-            self._start_perf_counter('mark_ticket_not_accessible_on_mongodb')
             self.tickets_repository.mark_not_accessible(ticket_id=ticket_id)
-            self._stop_perf_counter('mark_ticket_not_accessible_on_mongodb')
