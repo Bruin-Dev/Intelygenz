7aa9661bfb42e71354a23e103bcc7be557a3d347
David García <david.garciaguerrero@intelygenz.com>
2022-02-14 13:56:24 +0100

    fix(email-tagger-monitor): Update local script and local data to simulate webhooks

--- services/email-tagger-monitor/scripts/dev_notify_email.sh
+++ services/email-tagger-monitor/scripts/dev_notify_email.sh
@@ -1,30 +1,26 @@
 #!/bin/sh
 
-
 JSON_FILE=$1
 
-# public alias
-export HASH="email-tagger-dev"
-# export HASH="ff086d74"
+if [ $# -eq 0 ]
+  then
+    echo "No json file supplied"
+    echo "Example:"
+    echo "> ./local_notify_email.sh test_email.json"
+    exit 1
+fi
 
-# export REQUEST_API_KEY="vcY5L1lfsQEHcUolkd928FU2tAU2aB2NCmutMlV77bA="
-export REQUEST_API_KEY=""
+
 export SIGNATURE_SECRET_KEY="dev-shared-secret-body-signature"
-export EMAIL_WEBHOOK_URL="https://${HASH}.mettel-automation.net/api/email-tagger-webhook/email"
+export EMAIL_WEBHOOK_URL="http://localhost:5055/api/email-tagger-webhook/email"
 
 PAYLOAD=$(cat $JSON_FILE | python scripts/randomize.py)
 SIGNATURE=$(echo $PAYLOAD | python scripts/signature.py)
-
-# Uncomment to enable API KEY header
-# API_KEY_HEADER="-H \"api-key: ${REQUEST_API_KEY}\""
-API_KEY_HEADER=""
 
 echo "PAYLOAD: ${PAYLOAD}"
 echo "SIGNATURE: ${SIGNATURE}"
 
 curl -i -XPOST \
   -H 'content-type: application/json' \
-  $API_KEY_HEADER \
   -H "x-bruin-webhook-signature: ${SIGNATURE}" \
   --data "${PAYLOAD}"  ${EMAIL_WEBHOOK_URL}
-
--- services/email-tagger-monitor/scripts/randomize.py
+++ services/email-tagger-monitor/scripts/randomize.py
@@ -7,23 +7,20 @@ import string
 data = sys.stdin.readlines()
 data = json.loads("".join(data).replace("\n", ""))
 
-alphabet = string.ascii_uppercase + string.digits
-
 body = data['Notification']['Body']
 
 
-def randomize(input):
-    return ''.join(random.choices(alphabet, k=len(str(input))))
-
+def number_randomize(input):
+    return ''.join(random.choices(string.digits, k=len(str(input))))
 
 if 'Ticket' in body:
-    body['Ticket']["TicketID"] = randomize(body['Ticket']["TicketID"])
+    body['Ticket']["TicketID"] = number_randomize(body['Ticket']["TicketID"])
 
-body["EmailId"] = randomize(body["EmailId"])
-body["ClientId"] = randomize(body["ClientId"])
+body["EmailId"] = number_randomize(body["EmailId"])
+body["ClientId"] = number_randomize(body["ClientId"])
 
 if random.randint(0, 100) > 90:
-    body["ParentID"] = randomize(body["EmailId"])
+    body["ParentID"] = number_randomize(body["EmailId"])
 else:
     body.pop("ParentID", None)
 
