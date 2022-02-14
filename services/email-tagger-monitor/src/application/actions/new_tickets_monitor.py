106a1e725a8350bac8e19f35b70dca20586c5706
David García <david.garciaguerrero@intelygenz.com>
2022-02-14 13:58:23 +0100

    fix(email-tagger-monitor): Add TODO comments in the code

--- services/email-tagger-monitor/src/application/actions/new_tickets_monitor.py
+++ services/email-tagger-monitor/src/application/actions/new_tickets_monitor.py
@@ -59,6 +59,8 @@ class NewTicketsMonitor:
             for data in new_tickets
             if self._new_tickets_repository.validate_ticket(data)
         ]
+        # TODO: We have no visibility over this taks results. Errors happend and we will not receive anything
+        # TODO: when some errors occur we are breaking the task loop and it no running anymore until restarting the service
         await asyncio.gather(*tasks, return_exceptions=True)
         self._logger.info("NewTicketsMonitor process finished! Took {:.3f}s".format(time.time() - start_time))
 
@@ -74,6 +76,7 @@ class NewTicketsMonitor:
     async def _save_metrics(self, email_data: dict, ticket_data: dict):
         email_id = email_data["email"]["email_id"]
         ticket_id = int(ticket_data["ticket_id"])
+        self._logger.info(f"2 - Running save metric: {email_id} - {ticket_id}")
 
         async with self._semaphore:
             self._logger.info("Go in to sem")
