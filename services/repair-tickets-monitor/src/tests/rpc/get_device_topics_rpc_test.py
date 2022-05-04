f25b89e2f3ee0f639969952a53b584544249f041
Gabriel Palacios <gabriel.palacios@intelygenz.com>
2022-05-04 14:11:39 +0200

    feat(repair-tickets-monitor): remove redundant tests and update test values

--- services/repair-tickets-monitor/src/tests/rpc/get_device_topics_rpc_test.py
+++ services/repair-tickets-monitor/src/tests/rpc/get_device_topics_rpc_test.py
@@ -1,5 +1,5 @@
 from logging import Logger
-from typing import Dict, Any, Callable
+from typing import Dict, Any, Callable, Optional
 from unittest.mock import Mock, ANY
 
 from asynctest import CoroutineMock
@@ -9,54 +9,25 @@ from pytest import fixture, mark, raises
 from application.domain.device import Device
 from application.domain.topic import Topic
 from application.rpc.base_rpc import RpcLogger, RpcError, RpcResponse, OK_STATUS
-from application.rpc.get_device_topics_rpc import GetDeviceTopicsRpc, DeviceTopicsRequest, CallType, DeviceTopicsBody
+from application.rpc.get_device_topics_rpc import GetDeviceTopicsRpc, DeviceTopicsRequest, DeviceTopicsBody
 
 
 class TestDeviceTopicsRequest:
-    def instance_test(self):
-        request_id = Mock(str)
-        client_id = Mock(int)
-        service_number = Mock(str)
-
-        subject = DeviceTopicsRequest(request_id=request_id, client_id=client_id, service_number=service_number)
-
-        assert subject.request_id == request_id
-        assert subject.client_id == client_id
-        assert subject.service_number == service_number
-
     def requests_are_properly_built_from_devices_test(self, a_device):
-        request_id = Mock(str)
+        request_id = "any_request_id"
         subject = DeviceTopicsRequest.from_device(request_id=request_id, device=a_device)
 
-        assert subject == DeviceTopicsRequest.construct(
+        assert subject == DeviceTopicsRequest(
             request_id=request_id,
             client_id=a_device.client_id,
             service_number=a_device.service_number
         )
 
 
-class TestCallType:
-    def instance_test(self):
-        call_type = Mock(str)
-        category = Mock(str)
-
-        subject = CallType(callType=call_type, category=category)
-
-        assert subject.call_type == call_type
-        assert subject.category == category
-
-
 class TestDeviceTopicsBody:
-    def instance_test(self):
-        call_types = []
-
-        subject = DeviceTopicsBody(callTypes=call_types)
-
-        assert subject.call_types == call_types
-
     def model_parses_only_valid_items_test(self):
-        call_type = Mock(str)
-        category = Mock(str)
+        call_type = "any_call_type"
+        category = "any_category"
         subject = DeviceTopicsBody.parse_obj({
             "callTypes": [
                 {
@@ -82,14 +53,11 @@ class TestDeviceTopicsBody:
 
 class TestGetDeviceTopicsRpc:
     @mark.asyncio
-    async def messages_are_properly_sent_test(self, get_device_topics_rpc_builder):
+    async def messages_are_properly_sent_test(self, get_device_topics_rpc_builder, a_device):
         # given
-        client_id = Mock(int)
-        service_number = Mock(str)
-        call_type = Mock(str)
-        category = Mock(str)
+        call_type = "any_call_type"
+        category = "any_category"
 
-        device = Device(service_number=service_number, client_id=client_id)
         get_device_topics_rpc = get_device_topics_rpc_builder()
         get_device_topics_rpc.send = CoroutineMock(return_value=RpcResponse(
             status=200,
@@ -102,12 +70,16 @@ class TestGetDeviceTopicsRpc:
         ))
 
         # when
-        subject = await get_device_topics_rpc(device)
+        subject = await get_device_topics_rpc(a_device)
 
         # then
         assert subject == [Topic(call_type=call_type, category=category)]
         get_device_topics_rpc.send.assert_awaited_once_with(
-            DeviceTopicsRequest.construct(service_number=service_number, client_id=client_id, request_id=ANY)
+            DeviceTopicsRequest.construct(
+                service_number=a_device.service_number,
+                client_id=a_device.client_id,
+                request_id=ANY
+            )
         )
 
     @mark.asyncio
@@ -125,16 +97,19 @@ class TestGetDeviceTopicsRpc:
 
 @fixture
 def a_device() -> Device:
-    return Device(service_number=Mock(str), client_id=Mock(int))
+    return Device(service_number="a_service_number", client_id=1)
 
 
 @fixture
 def rpc_response_builder() -> Callable[..., RpcResponse]:
     def builder(
         status: int = OK_STATUS,
-        body: Dict[str, Any] = Mock(dict)
+        body: Optional[Dict[str, Any]] = None
     ):
-        return RpcResponse(status, body)
+        if body is None:
+            body = {}
+
+        return RpcResponse.construct(status=status, body=body)
 
     return builder
 
@@ -143,12 +118,12 @@ def rpc_response_builder() -> Callable[..., RpcResponse]:
 def get_device_topics_rpc_builder() -> Callable[..., GetDeviceTopicsRpc]:
     def builder(
         event_bus: EventBus = Mock(EventBus),
-        topic: str = Mock(str),
-        timeout: int = Mock(int),
+        topic: str = "any_topic",
+        timeout: int = 1,
         logger: Logger = Mock(Logger)
     ):
         get_device_topics_rpc = GetDeviceTopicsRpc(event_bus, topic, timeout, logger)
-        get_device_topics_rpc.start = Mock(return_value=(Mock(str), Mock(RpcLogger)))
+        get_device_topics_rpc.start = Mock(return_value=("a_request_id", Mock(RpcLogger)))
         return get_device_topics_rpc
 
     return builder
