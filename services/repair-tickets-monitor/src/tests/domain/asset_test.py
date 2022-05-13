d154c8a2207bbd904bfe2e630156ab1d070cbf6f
Gabriel Palacios <gabriel.palacios@intelygenz.com>
2022-05-13 10:28:21 +0200

    feat(repair-tickets-monitor): fix test

--- services/repair-tickets-monitor/src/tests/domain/asset_test.py
+++ services/repair-tickets-monitor/src/tests/domain/asset_test.py
@@ -8,16 +8,16 @@ class TestAsset:
         topic_a = Topic(call_type=ANY, category="A")
         topic_b = Topic(call_type=ANY, category="B")
         topic_c = Topic(call_type=ANY, category="C")
-        asset_1 = Asset(id=ANY, topics=[topic_a])
-        asset_2 = Asset(id=ANY, topics=[topic_a, topic_b])
-        asset_3 = Asset(id=ANY, topics=[topic_b])
-        asset_4 = Asset(id=ANY, topics=[topic_b, topic_c])
+        asset_1 = Asset(id=ANY, allowed_topics=[topic_a])
+        asset_2 = Asset(id=ANY, allowed_topics=[topic_a, topic_b])
+        asset_3 = Asset(id=ANY, allowed_topics=[topic_b])
+        asset_4 = Asset(id=ANY, allowed_topics=[topic_b, topic_c])
 
         assets = Assets()
-        assets.append(Asset(id=ANY, topics=[topic_a]))
-        assets.append(Asset(id=ANY, topics=[topic_a, topic_b]))
-        assets.append(Asset(id=ANY, topics=[topic_b]))
-        assets.append(Asset(id=ANY, topics=[topic_b, topic_c]))
+        assets.append(Asset(id=ANY, allowed_topics=[topic_a]))
+        assets.append(Asset(id=ANY, allowed_topics=[topic_a, topic_b]))
+        assets.append(Asset(id=ANY, allowed_topics=[topic_b]))
+        assets.append(Asset(id=ANY, allowed_topics=[topic_b, topic_c]))
 
         assert assets.get_allowed_for("A") == [asset_1, asset_2]
         assert assets.get_allowed_for("B") == [asset_2, asset_3, asset_4]
