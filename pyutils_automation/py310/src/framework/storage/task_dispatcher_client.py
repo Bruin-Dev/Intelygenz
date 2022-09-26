faf266a39e114ec5011f0a3e744229da3fbfed83
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2022-09-26 00:43:43 -0700

    feat(task-dispatcher): add a `schedule_task` function to the `task_dispatcher_client`

--- pyutils_automation/py310/src/framework/storage/task_dispatcher_client.py
+++ pyutils_automation/py310/src/framework/storage/task_dispatcher_client.py
@@ -15,6 +15,14 @@ class TaskDispatcherClient:
 
         self._redis_key_prefix = f"{config.ENVIRONMENT_NAME}-task_dispatcher"
 
+    def schedule_task(self, date: datetime, task_type: TaskTypes, task_key: str, task_data: dict):
+        zkey = f"{self._redis_key_prefix}-z-{task_type.value}"
+        hkey = f"{self._redis_key_prefix}-h-{task_type.value}"
+        timestamp = date.timestamp()
+
+        self._redis.zadd(zkey, {task_key: timestamp})
+        self._redis.hset(hkey, task_key, json.dumps(task_data))
+
     def clear_task(self, task_type: TaskTypes, task_key: str) -> int:
         zkey = f"{self._redis_key_prefix}-z-{task_type.value}"
         hkey = f"{self._redis_key_prefix}-h-{task_type.value}"
