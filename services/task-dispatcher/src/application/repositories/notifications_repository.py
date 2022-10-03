a56836173a9f47cbbf59f76c20f8ca3861618a2b
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2022-10-03 16:38:45 -0700

    fix(task-dispatcher): pass the correct request body to send a slack message

--- services/task-dispatcher/src/application/repositories/notifications_repository.py
+++ services/task-dispatcher/src/application/repositories/notifications_repository.py
@@ -15,6 +15,6 @@ class NotificationsRepository:
     async def send_slack_message(self, message: str):
         message = {
             "request_id": uuid(),
-            "body": f"[{self._config.LOG_CONFIG['name']}]: {message}",
+            "body": {"message": f"[{self._config.LOG_CONFIG['name']}]: {message}"},
         }
         await self._nats_client.request("notification.slack.request", to_json_bytes(message), timeout=10)
