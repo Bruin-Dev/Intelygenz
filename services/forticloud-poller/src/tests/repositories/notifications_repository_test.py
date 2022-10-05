3fc571439e7389c5fd4f0cacc5501cd797a1ef41
javier.aguilera <javier.aguilera@intelygenz.com>
2022-10-05 10:20:38 +0200

    fix(forticloud-poller): fix test

--- services/forticloud-poller/src/tests/repositories/notifications_repository_test.py
+++ services/forticloud-poller/src/tests/repositories/notifications_repository_test.py
@@ -1,3 +1,5 @@
+import json
+from typing import Any
 from unittest.mock import AsyncMock, patch
 
 import pytest
@@ -10,19 +12,25 @@ uuid_mock = patch.object(notifications_repository_module, "uuid", return_value=u
 
 
 class TestNotificationsRepository:
+    @staticmethod
+    def to_json_bytes(message: dict[str, Any]):
+        return json.dumps(message, default=str, separators=(",", ":")).encode()
+
     @pytest.mark.asyncio
     async def send_slack_message_test(self, notifications_repository):
         message = "Some message"
-        notifications_repository._nats_client.rpc_request = AsyncMock()
+        notifications_repository._nats_client.request = AsyncMock()
 
         with uuid_mock:
             await notifications_repository.send_slack_message(message)
 
-        notifications_repository._nats_client.rpc_request.assert_awaited_once_with(
+        notifications_repository._nats_client.request.assert_awaited_once_with(
             "notification.slack.request",
-            {
+            self.to_json_bytes({
                 "request_id": uuid_,
-                "body": {"message": f"[{notifications_repository._config.LOG_CONFIG['name']}]: {message}"},
-            },
+                "body": {
+                    "message": f"[{notifications_repository._config.LOG_CONFIG['name']}]: {message}"
+                },
+            }),
             timeout=10,
         )
