7c45e147a437da1df63b83632633807d7056a11d
javier.aguilera <javier.aguilera@intelygenz.com>
2022-10-05 19:04:49 +0200

    fix(forticloud-cache): fix terraform file

--- infra-as-code/basic-infra/1-parameters/forticloud-cache.tf
+++ infra-as-code/basic-infra/1-parameters/forticloud-cache.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter-forticloud-cache-time-to-refresh-interval" {
-    name        = "/automation-engine/${local.env}/forticloud-cache/time-to-refresh-interval"
+    count       = var.CURRENT_ENVIRONMENT == "dev" ? 1 : 0   # -> use this to deploy a "common" parameter only in one environment, if not when merging to master will fail for duplicity
+    name        = "/automation-engine/common/forticloud-cache/time-to-refresh-interval"
     description = "Time to refresh the Forticloud cache "
     type        = "SecureString"
     value       = "-"  # to edit go to parameter store dashboard.
@@ -17,10 +18,11 @@ resource "aws_ssm_parameter" "parameter-forticloud-cache-time-to-refresh-interva
     })
 }
 resource "aws_ssm_parameter" "parameter-forticloud-cache-monitorable-management-status" {
-    name = "/automation-engine/${local.env}/forticloud-cache/monitorable-management-status"
+    count       = var.CURRENT_ENVIRONMENT == "dev" ? 1 : 0   # -> use this to deploy a "common" parameter only in one environment, if not when merging to master will fail for duplicity
+    name        = "/automation-engine/common/forticloud-cache/monitorable-management-status"
     description = "Managements statuses to store devices in cache "
-    type = "SecureString"
-    value = "-"
+    type        = "SecureString"
+    value       = "-"
     # to edit go to parameter store dashboard.
     key_id = aws_kms_alias.kms_key.name
 
