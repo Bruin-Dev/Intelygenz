a6897ee2032f1abd323244f21c2adb97efea8ccb
kelly migap <kelly.migap@intelygenz.com>
2022-10-14 14:51:20 -0500

    Build(infra-as-code): Modify patterns in  Metrics and Alarms created for CloudWatch

---
+++ infra-as-code/basic-infra/8-metrics-and-alarms/intermapper_outage__events_processed.tf
@@ -0,0 +1,36 @@
+resource "aws_cloudwatch_log_metric_filter" "intermapper_outage__events_processed" {
+  name           = "intermapper_outage__events_processed"
+  pattern        = "{ $.environment = \"production\" && $.hostname = \"intermapper-outage-monitor-*\" && $.message = \"Processing email with msg_uid:\" }"
+  log_group_name = aws_cloudwatch_log_group.eks_log_group.name
+
+  metric_transformation {
+    name      = "intermapper_outage__events_processed"
+    namespace = "mettel_automation/alarms"
+    value     = "$.message"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "intermapper-outage-no-events-processed-in-the-last-hour" {
+  alarm_name                = "intermapper-outage-no-events-processed-in-the-last-hour"
+  comparison_operator       = "LowerThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = aws_cloudwatch_log_metric_filter.intermapper_outage__events_processed.name
+  namespace                 = "mettel_automation/alarms"
+  period                    = "3600"
+  statistic                 = "Sum"
+  threshold                 = "0"
+  alarm_description         = "Triggers an alarm if no emails get into the inbox in the last 10 minutes"
+  insufficient_data_actions = []
+alarm_actions = []
+}
+
+resource "aws_sns_topic" "intermapper-outage-no-events-processed-in-the-last-hour" {
+  name = "intermapper-outage-no-events-processed-in-the-last-hour"
+}
+
+resource "aws_sns_topic_subscription" "intermapper-outage-no-events-processed-in-the-last-hour"{
+  for_each  = toset(["mettel.team@intelygenz.com"])
+  topic_arn = aws_sns_topic.intermapper-outage-no-events-processed-in-the-last-hour.arn
+  protocol = "email"
+  endpoint = each.value
+}
