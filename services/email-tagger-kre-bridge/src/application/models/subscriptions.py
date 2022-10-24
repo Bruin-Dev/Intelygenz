2b9f8dd25dbe0619a77b6e6fb86c4fc93b0108d8
javier.aguilera <javier.aguilera@intelygenz.com>
2022-10-24 15:31:56 +0200

    fix(email-tagger-kre-bridge): fix wrong subscriptions topics

--- services/email-tagger-kre-bridge/src/application/models/subscriptions.py
+++ services/email-tagger-kre-bridge/src/application/models/subscriptions.py
@@ -5,11 +5,11 @@ from framework.nats.models import Subscription
 
 @dataclass(kw_only=True)
 class GetPrediction(Subscription):
-    subject: str = "email_tagger.metrics.request"
+    subject: str = "email_tagger.prediction.request"
     queue: str = "email_tagger_kre_bridge"
 
 
 @dataclass(kw_only=True)
 class SaveMetrics(Subscription):
-    subject: str = "email_tagger.prediction.request"
+    subject: str = "email_tagger.metrics.request"
     queue: str = "email_tagger_kre_bridge"
