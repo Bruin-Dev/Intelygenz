34910e0e33f202ddf37784d3968c706e95f2c78b
Sergio del Puerto <sergio.delpuerto@intelygenz.com>
2022-10-27 14:59:28 -0700

    build(intermapper-outage-monitor): add a `MAX_EMAILS_TO_RETRIEVE` environment variable

--- infra-as-code/basic-infra/1-parameters/intermapper-outage-monitor.tf
+++ infra-as-code/basic-infra/1-parameters/intermapper-outage-monitor.tf
@@ -237,6 +237,26 @@ resource "aws_ssm_parameter" "parameter-intermapper-outage-monitor-events-lookup
   })
 }
 
+resource "aws_ssm_parameter" "parameter-intermapper-outage-monitor-max-emails-to-retrieve" {
+  count       = var.CURRENT_ENVIRONMENT == "dev" ? 1 : 0   # -> use this to deploy a "common" parameter only in one environment, if not when merging to master will fail for duplicity
+  name        = "/automation-engine/common/intermapper-outage-monitor/max-emails-to-retrieve"
+  description = "How many emails to retrieve at most for InterMapper events in the desired e-mail inbox"
+  type        = "SecureString"
+  value       = "-"  # to edit go to parameter store dashboard.
+  key_id      =  aws_kms_alias.kms_key.name
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+
+  tags = merge(var.common_info, {
+    Name = "MAX_EMAILS_TO_RETRIEVE"
+    note = "can be updated from the parameter store dashboard"
+  })
+}
+
 resource "aws_ssm_parameter" "parameter-intermapper-outage-monitor-monitor-piab-devices" {
   count       = var.CURRENT_ENVIRONMENT == "dev" ? 1 : 0   # -> use this to deploy a "common" parameter only in one environment, if not when merging to master will fail for duplicity
   name        = "/automation-engine/common/intermapper-outage-monitor/monitor-piab-devices"
