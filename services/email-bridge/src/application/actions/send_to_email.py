1ec58ee549d2236ff994dbe85ae7712a4157d235
Daniel Fernández <daniel.fernandez@intelygenz.com>
2022-10-27 12:35:09 +0200

    refactor(): update actions to send e-mail and Slack notifications to not follow the Request-Reply model anymore

--- services/email-bridge/src/application/actions/send_to_email.py
+++ services/email-bridge/src/application/actions/send_to_email.py
@@ -14,38 +14,22 @@ class SendToEmail:
 
     async def __call__(self, msg: Msg):
         payload = json.loads(msg.data)
-        response = {"request_id": payload["request_id"], "body": None, "status": None}
 
         if payload.get("body") is None:
             logger.error(f"Cannot send to email with {json.dumps(payload)}. Must include body in request")
-
-            response["status"] = 400
-            response["body"] = 'Must include "body" in request'
-            await msg.respond(json.dumps(response).encode())
             return
 
         if not all(key in payload["body"].keys() for key in ("email_data",)):
             logger.error(f"Cannot send to email with {json.dumps(payload)}. JSON malformed")
-
-            response["status"] = 400
-            response["body"] = 'Must include "email_data" in request body'
-            await msg.respond(json.dumps(response).encode())
             return
 
         email_data = payload["body"]["email_data"]
         if email_data == "":
             logger.error(f'Cannot send to email with {json.dumps(payload)}. Parameter "email_data" cannot be empty')
-
-            response["status"] = 400
-            response["body"] = 'Must include a valid "email_data" in request body'
-            await msg.respond(json.dumps(response).encode())
             return
 
         send_to_email_response = self._email_repository.send_to_email(email_data)
-        response["body"] = send_to_email_response["body"]
-        response["status"] = send_to_email_response["status"]
         logger.info(
             f"Received the following from attempting to send {json.dumps(payload)}"
             f" to email: {send_to_email_response['body']}"
         )
-        await msg.respond(json.dumps(response).encode())
--- services/notifications-bridge/src/application/actions/send_to_slack.py
+++ services/notifications-bridge/src/application/actions/send_to_slack.py
@@ -15,32 +15,15 @@ class SendToSlack:
     async def __call__(self, msg: Msg):
         payload = json.loads(msg.data)
 
-        response = {"body": None, "status": None}
-
         if payload.get("body") is None:
             logger.error(f"Cannot send to slack with {json.dumps(payload)}. JSON malformed")
-
-            response["status"] = 400
-            response["body"] = 'Must include "body" in request'
-            await msg.respond(json.dumps(response).encode())
             return
 
         if not all(key in payload["body"].keys() for key in ("message",)):
             logger.error(f'Cannot send to slack with {json.dumps(payload)}. Need parameters "message"')
-
-            response["status"] = 400
-            response["body"] = 'Must include "message" in request body'
-            await msg.respond(json.dumps(response).encode())
             return
 
         message = payload["body"]["message"]
         logger.info(f"Sending msg {message} to slack...")
-        slack_response = await self._slack_repository.send_to_slack(message)
-        response["body"] = slack_response.body
-        response["status"] = slack_response.status
-
-        await msg.respond(json.dumps(response).encode())
-        logger.info(
-            f"Notifications send to slack published in event bus for request {json.dumps(payload)}."
-            f" Message published was {response}"
-        )
+        await self._slack_repository.send_to_slack(message)
+        logger.info(f"Notifications send to slack for request {json.dumps(payload)}.")
