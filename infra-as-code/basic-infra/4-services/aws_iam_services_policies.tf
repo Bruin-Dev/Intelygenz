47a05c013ce62823aa7c1f19a561e7dafdf0fdd7
Alberto Iglesias Gallego <alberto.iglesias@intelygenz.com>
2023-02-24 04:16:35 -0500

    Upgrade terraform aws provider from version 3 to 4

--- infra-as-code/basic-infra/4-services/aws_iam_services_policies.tf
+++ infra-as-code/basic-infra/4-services/aws_iam_services_policies.tf
@@ -4,9 +4,12 @@ data "aws_iam_policy_document" "service_affecting_monitor_s3" {
       "s3:*",
     ]
 
-    principal = [
-      "arn:aws:iam::374050862540:group/Automation",
-    ]
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::374050862540:group/Automation"
+      ]
+    }
 
     resources = [
       "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/",
@@ -20,9 +23,14 @@ data "aws_iam_policy_document" "service_affecting_monitor_s3" {
     ]
 
     principal = [
-      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${aws_iam_user.service_affecting_monitor_s3.bucket}",
-    ]
 
+    ]
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${aws_iam_user.service_affecting_monitor_s3.bucket}",
+      ]
+    }
     resources = [
       "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/",
       "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/*",
--- infra-as-code/basic-infra/4-services/services.tf
+++ infra-as-code/basic-infra/4-services/services.tf
@@ -9,5 +9,5 @@ module "service_affecting_monitor" {
   service_affecting_monitor_s3_bucket_name = aws_s3_bucket.service_affecting_monitor.bucket
   service_affecting_monitor_s3_access_key  = aws_iam_access_key.service_affecting_monitor_s3.id
   service_affecting_monitor_s3_secret_key  = aws_iam_access_key.service_affecting_monitor_s3.secret
-  global_ssm_kms_name                      = data.aws_kms_key.ssm-kms-key.name
+  global_ssm_kms_name                      = data.aws_kms_key.ssm-kms-key.id
 }
