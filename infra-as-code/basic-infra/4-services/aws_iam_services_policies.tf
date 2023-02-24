2b4eb05b0f7862e3f0bcd991a2bb7adf2789024a
Alberto Iglesias Gallego <alberto.iglesias@intelygenz.com>
2023-02-24 03:11:55 -0500

    fix typo

--- infra-as-code/basic-infra/4-services/aws_iam_services_policies.tf
+++ infra-as-code/basic-infra/4-services/aws_iam_services_policies.tf
@@ -9,8 +9,8 @@ data "aws_iam_policy_document" "service_affecting_monitor_s3" {
     ]
 
     resources = [
-      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.name}/",
-      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.name}/*",
+      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/",
+      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/*",
     ]
   }
 
@@ -20,12 +20,12 @@ data "aws_iam_policy_document" "service_affecting_monitor_s3" {
     ]
 
     principal = [
-      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${aws_iam_user.service_affecting_monitor_s3.name}",
+      "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${aws_iam_user.service_affecting_monitor_s3.bucket}",
     ]
 
     resources = [
-      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.name}/",
-      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.name}/*",
+      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/",
+      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/*",
     ]
   }
 }
