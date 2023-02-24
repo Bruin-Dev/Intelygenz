c2f1d02209278eb98469169a2095f84722beb81c
Alberto Iglesias Gallego <alberto.iglesias@intelygenz.com>
2023-02-24 15:23:45 -0500

    remove principals

--- infra-as-code/basic-infra/4-services/aws_iam_services_policies.tf
+++ infra-as-code/basic-infra/4-services/aws_iam_services_policies.tf
@@ -1,33 +1,8 @@
 data "aws_iam_policy_document" "service_affecting_monitor_s3" {
   statement {
     actions = [
-      "s3:*",
-    ]
-
-    principals {
-      type = "AWS"
-      identifiers = [
-        "arn:aws:iam::374050862540:group/Automation"
-      ]
-    }
-
-    resources = [
-      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/",
-      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/*",
-    ]
-  }
-
-  statement {
-    actions = [
       "s3:PutObject",
     ]
-
-    principals {
-      type = "AWS"
-      identifiers = [
-        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/${aws_iam_user.service_affecting_monitor_s3.name}",
-      ]
-    }
     resources = [
       "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/",
       "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/*",
