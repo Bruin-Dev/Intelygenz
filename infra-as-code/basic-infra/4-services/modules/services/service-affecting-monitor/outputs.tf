03b63c3ffe49129ec2470f884b3e167f5d9c0c7d
Alberto Iglesias Gallego <alberto.iglesias@intelygenz.com>
2023-02-24 02:44:57 -0500

    fix typo

--- infra-as-code/basic-infra/4-services/modules/services/service-affecting-monitor/outputs.tf
+++ infra-as-code/basic-infra/4-services/modules/services/service-affecting-monitor/outputs.tf
@@ -1,18 +1,15 @@
 
 output "ssm_parameter_name_service_affecting_s3_service_account_access_key" {
-  description = aws_ssm_parameter.service-affecting-s3-service-account-access-key.description
   value       = aws_ssm_parameter.service-affecting-s3-service-account-access-key.name
   sensitive   = true
 }
 
 output "ssm_parameter_name_service_affecting_s3_service_account_secret_key" {
-  description = aws_ssm_parameter.service-affecting-s3-service-account-secret-key.description
   value       = aws_ssm_parameter.service-affecting-s3-service-account-secret-key.name
   sensitive   = true
 }
 
 output "ssm_parameter_name_service_affecting_s3_bucket_name" {
-  description = aws_ssm_parameter.service-affecting-s3-bucket-name.description
   value       = aws_ssm_parameter.service-affecting-s3-bucket-name.name
   sensitive   = true
 }
