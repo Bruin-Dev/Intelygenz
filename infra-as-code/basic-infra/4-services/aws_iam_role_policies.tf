fc101f6b08ef9e0acfa54bea1917b340e8cc4ce7
Alberto Iglesias Gallego <alberto.iglesias@intelygenz.com>
2023-02-24 15:23:45 -0500

    remove principals

---
+++ infra-as-code/basic-infra/4-services/aws_iam_role_policies.tf
@@ -0,0 +1,22 @@
+data "aws_iam_role" "automation" {
+  name = "Automation"
+}
+
+resource "aws_iam_role_policy" "test_policy" {
+  name   = "s3_service_affecting_monitor"
+  role   = data.aws_iam_role.automation.id
+  policy = aws_iam_policy_document.service_affecting_monitor_s3_role.json
+}
+
+data "aws_iam_policy_document" "service_affecting_monitor_s3_role" {
+  statement {
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/",
+      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/*",
+    ]
+  }
+}
