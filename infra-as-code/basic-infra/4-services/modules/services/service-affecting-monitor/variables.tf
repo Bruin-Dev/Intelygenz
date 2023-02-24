063bec297810baf36f706de67c6ccaf70c58427f
Alberto Iglesias Gallego <alberto.iglesias@intelygenz.com>
2023-02-24 03:02:08 -0500

    fix typo

--- infra-as-code/basic-infra/4-services/modules/services/service-affecting-monitor/variables.tf
+++ infra-as-code/basic-infra/4-services/modules/services/service-affecting-monitor/variables.tf
@@ -3,25 +3,25 @@ variable "common_info" {
 }
 
 variable "service_affecting_monitor_s3_bucket_name" {
-  descreption = "Service Affection monitor s3 bucket name to put files in that bucket."
+  description = "Service Affection monitor s3 bucket name to put files in that bucket."
   sensitive   = true
   type        = string
 }
 
 variable "service_affecting_monitor_s3_access_key" {
-  descreption = "Service Affection monitor s3 service account access key."
+  description = "Service Affection monitor s3 service account access key."
   sensitive   = true
   type        = string
 }
 
 variable "service_affecting_monitor_s3_secret_key" {
-  descreption = "Service Affection monitor s3 service account secret key."
+  description = "Service Affection monitor s3 service account secret key."
   sensitive   = true
   type        = string
 }
 
 variable "global_ssm_kms_name" {
-  descreption = "Global KMS key name to encrypt SSM parameters."
+  description = "Global KMS key name to encrypt SSM parameters."
   sensitive   = true
   type        = string
 }
