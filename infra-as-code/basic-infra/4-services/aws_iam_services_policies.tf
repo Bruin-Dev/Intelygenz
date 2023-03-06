b6031887a3708d699dac330ce66dbf99320bd934
Alberto Iglesias Gallego <alberto.iglesias@intelygenz.com>
2023-03-05 23:55:05 -0500

    fix(policies): add kms access to service account

--- infra-as-code/basic-infra/4-services/aws_iam_services_policies.tf
+++ infra-as-code/basic-infra/4-services/aws_iam_services_policies.tf
@@ -8,4 +8,14 @@ data "aws_iam_policy_document" "service_affecting_monitor_s3" {
       "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/*",
     ]
   }
+
+  statement {
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey"
+    ]
+    resources = [
+      "*"
+    ]
+  }
 }
