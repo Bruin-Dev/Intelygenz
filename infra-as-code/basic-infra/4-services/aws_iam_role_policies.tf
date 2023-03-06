ad07f7502680682c9404a5c197b8b928a9f1b67f
Alberto Iglesias Gallego <alberto.iglesias@intelygenz.com>
2023-03-06 01:06:38 -0500

    fix(policies): add kms access to service account

--- infra-as-code/basic-infra/4-services/aws_iam_role_policies.tf
+++ infra-as-code/basic-infra/4-services/aws_iam_role_policies.tf
@@ -1,23 +1,23 @@
 
-data "aws_iam_group" "automation" {
-  group_name = "Automation"
-}
+#data "aws_iam_group" "automation" {
+#  group_name = "Automation"
+#}
+#
+#resource "aws_iam_group_policy" "automation_policy" {
+#  name   = "s3_service_affecting_monitor"
+#  group   = data.aws_iam_group.automation.group_id
+#  policy = data.aws_iam_policy_document.service_affecting_monitor_s3_role.json
+#}
 
-resource "aws_iam_group_policy" "automation_policy" {
-  name   = "s3_service_affecting_monitor"
-  group   = data.aws_iam_group.automation.id
-  policy = data.aws_iam_policy_document.service_affecting_monitor_s3_role.json
-}
-
-data "aws_iam_policy_document" "service_affecting_monitor_s3_role" {
-  statement {
-    actions = [
-      "s3:*",
-    ]
-
-    resources = [
-      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/",
-      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/*",
-    ]
-  }
-}
+#data "aws_iam_policy_document" "service_affecting_monitor_s3_role" {
+#  statement {
+#    actions = [
+#      "s3:*",
+#    ]
+#
+#    resources = [
+#      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/",
+#      "arn:aws:s3:::${aws_s3_bucket.service_affecting_monitor.bucket}/*",
+#    ]
+#  }
+#}
