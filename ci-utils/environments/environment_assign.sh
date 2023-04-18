b57a1d0607d34bddccaa0edb068686896679d6d1
Alberto Iglesias Gallego <alberto.iglesias@intelygenz.com>
2023-04-18 17:17:21 -0400

    fix(scripts): fix environmenr variables of semantic release on services scripts

--- ci-utils/environments/environment_assign.sh
+++ ci-utils/environments/environment_assign.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [[ "${CI_COMMIT_REF_SLUG}" != "master" ]]; then
+  export MODULE_NAME=$ECR_REPOSITORY_NAME
   export ENVIRONMENT_ID=$(echo -n "${CI_COMMIT_REF_SLUG}" | sha256sum | cut -c1-8)
   export ENVIRONMENT_VAR="automation-${ENVIRONMENT_ID}"
   export CURRENT_ENVIRONMENT="dev"
@@ -14,6 +15,7 @@ if [[ "${CI_COMMIT_REF_SLUG}" != "master" ]]; then
   export GRAFANA_INGRESS_ROOT_URL="https://${GRAFANA_INGRESS_HOSTNAME}"
   export ECR_REPOSITORY_TAG="${TAG}"
 else
+  export MODULE_NAME=$ECR_REPOSITORY_NAME
   export ENVIRONMENT_ID="production"
   export ENVIRONMENT_VAR="automation-master"
   export TAG=$(git tag | sort --version-sort | grep "^${ECR_REPOSITORY_NAME}" | tail -n -1| cut -d "@" -f2)
